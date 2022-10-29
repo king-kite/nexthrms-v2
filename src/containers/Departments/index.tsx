0ffbd98ceb5ee22234becf66470bd9cdd005e5f4
kingkite <kitewebdevelopers@gmail.com>
2022-10-29 11:10:49 +0100

    Edited the Departments index page

--- src/containers/Departments/index.tsx
+++ src/containers/Departments/index.tsx
@@ -76,7 +76,7 @@ const Departments = ({
 							caps: true,
 							disabled: isFetching,
 							iconLeft: FaSearch,
-							padding: 'pl-2 pr-4 py-[0.54rem]',
+							padding: 'pl-2 pr-4 py-[0.545rem]',
 							title: 'Search',
 							type: 'submit',
 						}}
