0231b1300edae6371cee308c52fb8e5c3342893c
kingkite <kitewebdevelopers@gmail.com>
2022-11-26 14:26:34 +0100

    Fixed bug in the media [...name.ts] api file

--- src/pages/api/media/[...name].ts
+++ src/pages/api/media/[...name].ts
@@ -3,26 +3,35 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import path from 'path';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	const { name, type } = req.query;
+	if (process.env.NODE_ENV === 'development') {
+		const { name, type } = req.query;
 
-	if (!name || name.length <= 0) {
-		return res.status(404).json({
-			status: 'error',
-			message: 'File was not found!',
-		});
-	}
+		if (!name || name.length <= 0) {
+			return res.status(404).json({
+				status: 'error',
+				message: 'File was not found!',
+			});
+		}
 
-	try {
-		const imagePath = (name as string[]).join('/');
-		const filePath = path.resolve(`media/${imagePath}`);
-		const imageBuffer = fs.readFileSync(filePath);
-		res.setHeader('Content-Type', type ? String(type) : 'image/*');
-		return res.status(200).send(imageBuffer);
-	} catch (error) {
-		// Send a default file
-		const filePath = path.resolve('public/logo.png');
-		const imageBuffer = fs.readFileSync(filePath);
-		res.setHeader('Content-Type', 'image/*');
-		return res.status(200).send(imageBuffer);
+		try {
+			const imagePath = (name as string[]).join('/');
+			const filePath = path.resolve(`media/${imagePath}`);
+			const imageBuffer = fs.readFileSync(filePath);
+			res.setHeader('Content-Type', type ? String(type) : 'image/*');
+			return res.status(200).send(imageBuffer);
+		} catch (error) {
+			// Send a default file
+			// const filePath = path.resolve('public/logo.png');
+			// const imageBuffer = fs.readFileSync(filePath);
+			// res.setHeader('Content-Type', 'image/*');
+			return res.status(500).json({
+				status: 'error',
+				message: 'Unable to load file!',
+			});
+		}
 	}
+	return res.status(404).json({
+		status: 'error',
+		message: 'Page Not FOund!',
+	});
 }
