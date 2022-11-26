0638d07d991e3c105be3ee6c2fcb6a27eb87f777
kingkite <kitewebdevelopers@gmail.com>
2022-11-26 14:47:05 +0100

    Changed the api media [...name].ts to an async function

--- src/pages/api/media/[...name].ts
+++ src/pages/api/media/[...name].ts
@@ -2,7 +2,24 @@ import fs from 'fs';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import path from 'path';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+async function asyncReadFile(filePath: string): Promise<Buffer> {
+	return new Promise((resolve, reject) => {
+		try {
+			const resolvedPath = path.resolve(`media/${filePath}`);
+			fs.readFile(resolvedPath, (err, data) => {
+				if (err) reject(err);
+				resolve(data);
+			});
+		} catch (error) {
+			reject(error);
+		}
+	});
+}
+
+export default async function handler(
+	req: NextApiRequest,
+	res: NextApiResponse
+) {
 	if (process.env.NODE_ENV === 'development') {
 		const { name, type } = req.query;
 
@@ -14,20 +31,23 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 		}
 
 		try {
-			const imagePath = (name as string[]).join('/');
-			const filePath = path.resolve(`media/${imagePath}`);
-			const imageBuffer = fs.readFileSync(filePath);
+			const filePath = (name as string[]).join('/');
+			const fileBuffer = await asyncReadFile(filePath);
 			res.setHeader('Content-Type', type ? String(type) : 'image/*');
-			return res.status(200).send(imageBuffer);
+			return res.status(200).send(fileBuffer);
 		} catch (error) {
-			// Send a default file
-			// const filePath = path.resolve('public/logo.png');
-			// const imageBuffer = fs.readFileSync(filePath);
-			// res.setHeader('Content-Type', 'image/*');
-			return res.status(500).json({
-				status: 'error',
-				message: 'Unable to load file!',
-			});
+			try {
+				// Send a default file
+				const filePath = path.resolve('public/logo.png');
+				const fileBuffer = await asyncReadFile(filePath);
+				res.setHeader('Content-Type', 'image/*');
+				return res.status(200).send(fileBuffer);
+			} catch (error) {
+				return res.status(500).json({
+					status: 'error',
+					message: 'Unable to load file!',
+				});
+			}
 		}
 	}
 	return res.status(404).json({
---
+++ src/pages/api/users/[id].ts
@@ -0,0 +1,161 @@
+import { Prisma } from '@prisma/client';
+
+import {
+	employeeSelectQuery as selectQuery,
+	getEmployee,
+	prisma,
+} from '../../../db';
+import { auth } from '../../../middlewares';
+import { CreateEmployeeQueryType } from '../../../types';
+import { upload as uploadFile } from '../../../utils/files';
+import parseForm from '../../../utils/parseForm';
+import { createEmployeeSchema } from '../../../validators';
+
+export const config = {
+	api: {
+		bodyParser: false,
+	},
+};
+
+export default auth()
+	.get(async (req, res) => {
+		const employee = await getEmployee(req.query.id as string);
+
+		if (!employee) {
+			return res.status(404).json({
+				status: 'success',
+				message: 'Employee with specified ID was not found!',
+			});
+		}
+
+		return res.status(200).json({
+			status: 'success',
+			message: 'Fetched employee successfully',
+			data: employee,
+		});
+	})
+	.put(async (req, res) => {
+		const { fields, files } = (await parseForm(req)) as {
+			files: any;
+			fields: any;
+		};
+		if (!fields.form) {
+			return res.status(400).json({
+				status: 'error',
+				message: "'form' field is required",
+			});
+		}
+		const form = JSON.parse(fields.form);
+
+		const valid: CreateEmployeeQueryType =
+			await createEmployeeSchema.validateAsync(form);
+		if (!valid.user && !valid.userId) {
+			return res.status(400).json({
+				status: 'error',
+				message: 'Provide either user object or userId.',
+			});
+		} else if (valid.user && valid.userId) {
+			return res.status(400).json({
+				status: 'error',
+				message: 'Provide either user object or userId. Set the former to null',
+			});
+		}
+		if (valid.user && files.image) {
+			// Upload a file to the bucket using firebase admin
+			try {
+				const name = (
+					valid.user.firstName +
+					'_' +
+					valid.user.lastName +
+					'_' +
+					valid.user.email
+				).toLowerCase();
+
+				const location = `media/users/profile/${name}`;
+
+				const result = await uploadFile({
+					file: files.image,
+					location,
+					type: 'image',
+				});
+
+				valid.user.profile.image = result.secure_url || result.url;
+				Object(valid.user.profile).imageStorageInfo = {
+					id: result.public_id,
+					name: result.original_filename,
+					type: result.resource_type,
+				};
+			} catch (error) {
+				if (process.env.NODE_ENV === 'development')
+					console.log('EMPLOYEE UPDATE IMAGE ERROR :>> ', error);
+			}
+		}
+		const user: {
+			update?: Prisma.UserUpdateInput;
+			connect?: { id: string };
+		} = valid.user
+			? {
+					update: {
+						...valid.user,
+						email: valid.user.email.trim().toLowerCase(),
+						profile: {
+							update: valid.user.profile,
+						},
+					},
+			  }
+			: valid.userId
+			? {
+					connect: {
+						id: valid.userId,
+					},
+			  }
+			: {};
+
+		const data: Prisma.EmployeeUpdateInput = {
+			dateEmployed: valid.dateEmployed || new Date(),
+			department: {
+				connect: {
+					id: valid.department,
+				},
+			},
+			job: {
+				connect: {
+					id: valid.job,
+				},
+			},
+			supervisor: valid.supervisor
+				? {
+						connect: {
+							id: valid.supervisor,
+						},
+				  }
+				: undefined,
+			user,
+		};
+
+		const employee = await prisma.employee.update({
+			where: {
+				id: req.query.id as string,
+			},
+			data,
+			select: selectQuery,
+		});
+
+		return res.status(200).json({
+			status: 'success',
+			message: 'Employee was updated successfully',
+			data: employee,
+		});
+	})
+	.delete(async (req, res) => {
+		await prisma.employee.delete({
+			where: {
+				id: req.query.id as string,
+			},
+		});
+
+		return res.status(200).json({
+			status: 'success',
+			message: 'Employee was deleted successfully!',
+		});
+	});
