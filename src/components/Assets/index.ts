eb40128b6646e2696a7917812347eb222e22910d
kingkite <kitewebdevelopers@gmail.com>
2022-12-10 22:06:42 +0100

    Export Details component

--- src/components/Assets/index.ts
+++ src/components/Assets/index.ts
@@ -1,3 +1,4 @@
+export { default as Details } from './Details';
 export { default as Form } from './Form';
 export { default as SearchForm } from './SearchForm';
 export { default as AssetTable } from './Table';
