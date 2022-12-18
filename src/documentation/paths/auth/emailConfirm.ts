d78765255f9a85e95d03cf652e3501502b57599e
kingkite <kitewebdevelopers@gmail.com>
2022-12-19 00:33:47 +0100

    added API Doc for confirming user email verification link:

---
+++ src/documentation/paths/auth/emailConfirm.ts
@@ -0,0 +1,77 @@
+import responses from "../../responses";
+
+const path ={
+	post: {
+		// description: 'Sign In User',
+		requestBody: {
+			required: true,
+			content: {
+				"application/json": {
+					schema: {
+						type: "object",
+						properties: {
+							uid: {
+								type: "string",
+								format: "uuid",
+							},
+							token: {
+								type: "string"
+							}
+						},
+						example: {
+							uid: "58aa746a-5973-4e80-a635-db4eff513a2a",
+							token: "9153d2144ec53102b8d64c51209a745b6f5c67f555d0085c8c2ee90a9645fce26f506ce1641948cab7afd16ea7c04c0c"
+						},
+					},
+				},
+			},
+		},
+		responses: {
+			...responses,
+			"200": {
+				content: {
+					"application/json": {
+						schema: {
+							$ref: "#/components/schemas/BaseModel",
+						},
+					}
+				},
+				description: "Email Verification Link Confirmed",
+			},
+			"400": {
+				content: {
+					"application/json": {
+						schema: {
+							allOf: [
+								{$ref: "#/components/schemas/BaseModel"},
+								{
+									type: "object",
+									properties: {
+										error: {
+											type: "object",
+											nullable: true,
+											properties: {
+												token: {
+													type: 'string',
+													nullable: true
+												}
+											}
+										}
+									},
+									nullable: true
+								}
+							]
+						}
+					}
+				},
+				description: "Bad Request"
+			},
+			"307": undefined,
+			"401": undefined
+		},
+		summary: "Confirm User Email Verification Link",
+		tags: ["Authentication"],
+	},
+}
+
+export default path;
--- src/documentation/paths/auth/index.ts
+++ src/documentation/paths/auth/index.ts
@@ -1,17 +1,21 @@
-import emailResend from './emailResend';
-import login from './login';
-import logout from './logout';
-import profile from './profile';
-import register from './register';
-import user from './user';
+import * as routes from "../../../config/server";
+
+import emailConfirm from "./emailConfirm";
+import emailResend from "./emailResend";
+import login from "./login";
+import logout from "./logout";
+import profile from "./profile";
+import register from "./register";
+import user from "./user";
 
 const paths = {
-	"/api/auth/register/": register,
-	"/api/auth/email/resend/": emailResend,
-	"/api/auth/login/": login,
-	"/api/auth/logout/": logout,
-	"/api/auth/profile/": profile,
-	"/api/auth/user/": user,
-}
+	[routes.REGISTER_URL]: register,
+	[routes.EMAIL_RESEND_URL]: emailResend,
+	[routes.EMAIL_CONFIRM_URL]: emailConfirm,
+	[routes.LOGIN_URL]: login,
+	[routes.LOGOUT_URL]: logout,
+	[routes.USER_DATA_URL]: user,
+	[routes.PROFILE_URL]: profile,
+};
 
-export default paths
+export default paths;
