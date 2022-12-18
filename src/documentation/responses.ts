dc513422e1b8a3d75c2762aa1d5280bfd51a53f8
kingkite <kitewebdevelopers@gmail.com>
2022-12-18 20:23:12 +0100

    Added the default responses

--- src/documentation/responses.ts
+++ src/documentation/responses.ts
@@ -1,3 +1,34 @@
-const responses = {}
+const responses = {
+	"307": {
+		content: {
+			"application/json": {
+				schema: {
+					$ref: "#/components/schemas/BaseErrorRedirectModel"
+				}
+			}
+		},
+		description: "Redirect the user to another page"
+	},
+	"401": {
+		content: {
+			"application/json": {
+				schema: {
+					$ref: "#/components/schemas/BaseErrorModel",
+				}
+			}
+		},
+		description: "Authentication Failed"
+	},
+	"500": {
+		content: {
+			"application/json": {
+				schema: {
+					$ref: "#/components/schemas/BaseErrorModel"
+				}
+			}
+		},
+		description: "Internal Server Error"
+	}
+}
 
 export default responses
