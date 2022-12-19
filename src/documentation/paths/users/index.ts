d3c257c2262966eb49d9c9c73de321d038cde709
kingkite <kitewebdevelopers@gmail.com>
2022-12-19 21:12:02 +0100

    Added API Doc for getting single user, updating single user and deleting single user

--- src/documentation/paths/users/index.ts
+++ src/documentation/paths/users/index.ts
@@ -1,9 +1,11 @@
 import * as routes from "../../../config/server";
 
+import user from "./user"; // Single User Path
 import users from "./users";
 
 const paths = {
 	[routes.USERS_URL]: users,
+	[routes.USER_URL("{id}")]: user
 }
 
 export default paths;
