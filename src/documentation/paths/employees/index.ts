c79ca7abe2c9bb303ad8f1a1659e3b7e15d6f523
kingkite <kitewebdevelopers@gmail.com>
2022-12-20 11:47:56 +0100

    Added API DOC for updating single employee

--- src/documentation/paths/employees/index.ts
+++ src/documentation/paths/employees/index.ts
@@ -1,10 +1,12 @@
 import * as routes from "../../../config/server";
 
 import employeesExport from './exportEmployees'
+import employee from './employee'; // Single Employee
 import employees from './employees'
 
 const paths = {
 	[routes.EMPLOYEES_URL]: employees,
+	[routes.EMPLOYEE_URL("{id}")]: employee,
 	[routes.EMPLOYEES_EXPORT_URL]: employeesExport,
 };
 
