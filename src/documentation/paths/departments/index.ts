f4f9f97e4ad92b7ce397f82de869bbf02a066ab9
Emmanuel <emmanuel.kolade1@gmail.com>
2022-12-21 14:23:34 +0000

    Added API Doc for deleting a department, getting a single deparmtnet and updating a department

--- src/documentation/paths/departments/index.ts
+++ src/documentation/paths/departments/index.ts
@@ -1,10 +1,12 @@
 import * as routes from '../../../config/server';
 
+import department from './department';
 import departments from './departments';
 import exportDepartments from './exportDepartments';
 
 const path = {
     [routes.DEPARTMENTS_URL]: departments,
+    [routes.DEPARTMENT_URL("{id}")]: department,
     [routes.DEPARTMENTS_EXPORT_URL]: exportDepartments,
 };
 
