8612c60e533b67df08c7bbc3e9e8806c894008b9
Emmanuel <emmanuel.kolade1@gmail.com>
2022-12-22 11:27:09 +0000

    Add API Doc for Updating leave request

--- src/documentation/paths/leaves/leave.ts
+++ src/documentation/paths/leaves/leave.ts
@@ -2,6 +2,8 @@ import responses from '../../responses';
 import * as refs from '../../refs';
 import * as tags from '../../tags';
 
+import { requestExample, requestProperties } from './leaves';
+
 const path = {
 	get: {
 		parameters: [
@@ -40,6 +42,81 @@ const path = {
 		summary: 'Get Single Leave',
 		tags: [tags.Leaves],
 	},
+	put: {
+		parameters: [
+			{
+				in: 'path',
+				name: 'id',
+				required: true,
+				schema: {
+					type: 'string',
+					format: 'uuid',
+				},
+			},
+		],
+		requestBody: {
+			required: true,
+			content: {
+				'application/json': {
+					schema: {
+						properties: requestProperties,
+						example: requestExample,
+					},
+				},
+			},
+		},
+		responses: {
+			...responses,
+			'200': {
+				content: {
+					'application/json': {
+						schema: {
+							allOf: [
+								{ $ref: refs.BASE },
+								{
+									type: 'object',
+									properties: {
+										data: {
+											$ref: refs.LEAVE,
+										},
+									},
+								},
+							],
+						},
+					},
+				},
+			},
+			'400': {
+				content: {
+					'application/json': {
+						schema: {
+							type: 'object',
+							properties: {
+								type: {
+									type: 'string',
+									nullable: true,
+								},
+								startDate: {
+									type: 'string',
+									nullable: true,
+								},
+								endDate: {
+									type: 'string',
+									nullable: true,
+								},
+								reason: {
+									type: 'string',
+									nullable: true,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		summary: 'Update Single Leave',
+		tags: [tags.Leaves],
+	},
 	delete: {
 		parameters: [
 			{
@@ -58,14 +135,14 @@ const path = {
 				content: {
 					'application/json': {
 						schema: {
-							$ref: refs.BASE
-						}
-					}
-				}
-			}
+							$ref: refs.BASE,
+						},
+					},
+				},
+			},
 		},
 		summary: 'Delete Single Leave',
-		tags: [tags.Leaves]
+		tags: [tags.Leaves],
 	},
 };
 
