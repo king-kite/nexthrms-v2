15035e70207b8d03fcc494a5dcf79c43307f5257
Emmanuel <emmanuel.kolade1@gmail.com>
2022-12-22 14:02:07 +0000

    Added API oc for getting, updating, and deleting asingle overtime

---
+++ src/documentation/paths/overtime/adminOvertime.ts
@@ -0,0 +1,246 @@
+import responses from '../../responses';
+import * as refs from '../../refs';
+import * as tags from '../../tags';
+
+import { requestExample, requestProperties } from './adminAllOvertime';
+
+const path = {
+	get: {
+		parameters: [
+			{
+				in: 'path',
+				name: 'id',
+				required: true,
+				schema: {
+					type: 'string',
+					format: 'uuid',
+				},
+			},
+		],
+		responses: {
+			...responses,
+			'200': {
+				content: {
+					'application/json': {
+						schema: {
+							allOf: [
+								{ $ref: refs.BASE },
+								{
+									type: 'object',
+									properties: {
+										data: {
+											$ref: refs.OVERTIME,
+										},
+									},
+								},
+							],
+						},
+					},
+				},
+			},
+		},
+		summary: 'Admin Get Single Overtime',
+		tags: [tags.Overtime],
+	},
+	post: {
+		parameters: [
+			{
+				in: 'path',
+				name: 'id',
+				required: true,
+				schema: {
+					type: 'string',
+					format: 'uuid',
+				},
+			},
+		],
+		requestBody: {
+			required: true,
+			content: {
+				'application/json': {
+					schema: {
+						type: 'object',
+						required: ['approval'],
+						properties: {
+							approval: {
+								type: 'string',
+								format: "'APPROVED' | 'DENIED'",
+							},
+						},
+						example: {
+							approval: 'DENIED',
+						},
+					},
+				},
+			},
+		},
+		responses: {
+			...responses,
+			'200': {
+				content: {
+					'application/json': {
+						schema: {
+							allOf: [
+								{ $ref: refs.BASE },
+								{
+									type: 'object',
+									properties: {
+										data: {
+											$ref: refs.OVERTIME,
+										},
+									},
+								},
+							],
+						},
+					},
+				},
+			},
+			'400': {
+				content: {
+					'application/json': {
+						schema: {
+							allOf: [
+								{ $ref: refs.BASE },
+								{
+									type: 'object',
+									properties: {
+										error: {
+											type: 'object',
+											properties: {
+												approval: {
+													type: 'string',
+													nullable: true,
+												},
+											},
+										},
+									},
+								},
+							],
+						},
+					},
+				},
+			},
+		},
+		summary: 'Admin Approve or Deny Single Overtime',
+		tags: [tags.Overtime],
+	},
+	put: {
+		parameters: [
+			{
+				in: 'path',
+				name: 'id',
+				required: true,
+				schema: {
+					type: 'string',
+					format: 'uuid',
+				},
+			},
+		],
+		requestBody: {
+			required: true,
+			content: {
+				'application/json': {
+					schema: {
+						type: 'object',
+						properties: requestProperties,
+						example: requestExample,
+					},
+				},
+			},
+		},
+		responses: {
+			...responses,
+			'200': {
+				content: {
+					'application/json': {
+						schema: {
+							allOf: [
+								{ $ref: refs.BASE },
+								{
+									type: 'object',
+									properties: {
+										data: {
+											$ref: refs.OVERTIME,
+										},
+									},
+								},
+							],
+						},
+					},
+				},
+			},
+			'400': {
+				content: {
+					'application/json': {
+						schema: {
+							allOf: [
+								{ $ref: refs.BASE },
+								{
+									type: 'object',
+									properties: {
+										error: {
+											type: 'object',
+											properties: {
+												type: {
+													type: 'string',
+													nullable: true,
+												},
+												date: {
+													type: 'string',
+													nullable: true,
+												},
+												hours: {
+													type: 'string',
+													nullable: true,
+												},
+												reason: {
+													type: 'string',
+													nullable: true,
+												},
+												employee: {
+													type: 'string',
+													nullable: true,
+												},
+											},
+										},
+									},
+								},
+							],
+						},
+					},
+				},
+			},
+		},
+		summary: 'Admin Update Single Overtime',
+		tags: [tags.Overtime],
+	},
+	delete: {
+		parameters: [
+			{
+				in: 'path',
+				name: 'id',
+				required: true,
+				schema: {
+					type: 'string',
+					format: 'uuid',
+				},
+			},
+		],
+		responses: {
+			...responses,
+			'200': {
+				content: {
+					'application/json': {
+						schema: {
+							$ref: refs.BASE,
+						},
+					},
+				},
+			},
+		},
+		summary: 'Admin Delete Single Overtime',
+		tags: [tags.Overtime],
+	},
+};
+
+export default path;
