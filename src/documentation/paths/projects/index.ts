53b6d8ac8f8b64d6ea9f8c6b4fa560abb5247095
kingkite <kitewebdevelopers@gmail.com>
2022-12-24 21:21:53 +0100

    Added API Doc for getting, updating and deleting single projects

--- src/documentation/paths/projects/index.ts
+++ src/documentation/paths/projects/index.ts
@@ -1,10 +1,12 @@
 import * as routes from '../../../config/server';
 
+import project from './project';
 import projects from './projects';
 import exportProjects from './exportProjects';
 
 const paths = {
 	[routes.PROJECTS_URL]: projects,
+	[routes.PROJECT_URL("{id}")]: project,
 	[routes.PROJECTS_EXPORT_URL]: exportProjects,
 }
 
