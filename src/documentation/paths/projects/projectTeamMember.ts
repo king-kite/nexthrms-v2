3d017f89fa2d9088164880079e22c84053d1d55d
kingkite <kitewebdevelopers@gmail.com>
2022-12-25 21:42:46 +0100

    Added API Doc for updating single project team member

--- src/documentation/paths/projects/projectTeamMember.ts
+++ src/documentation/paths/projects/projectTeamMember.ts
@@ -58,92 +58,116 @@ const path = {
 		summary: 'Get Single Project Team Member',
 		tags: [tags.Projects]
 	},
-	// put: {
-	// 	parameters: [
-	// 		{
-	// 			in: 'path',
-	// 			name: 'projectId',
-	// 			required: true,
-	// 			schema: {
-	// 				type: 'string',
-	// 				format: 'uuid'
-	// 			}
-	// 		},
-	// 		{
-	// 			in: 'path',
-	// 			name: 'memberId',
-	// 			required: true,
-	// 			schema: {
-	// 				type: 'string',
-	// 				format: 'uuid'
-	// 			}
-	// 		},
-	// 	],
-	// 	responses: {
-	// 		responses: {
-	// 		...responses,
-	// 		"200": {
-	// 			content: {
-	// 				"application/json": {
-	// 					schema: {
-	// 						allOf: [
-	// 							{$ref: refs.BASE},
-	// 							{
-	// 								type: 'object',
-	// 								properties: {
-	// 									data: {
-	// 										$ref: refs.PROJECT_TEAM
-	// 									}
-	// 								}
-	// 							}
-	// 						]
-	// 					}
-	// 				}
-	// 			}
-	// 		},
-	// 		"400": {
-	// 			content: {
-	// 				"application/json": {
-	// 					schema: {
-	// 						allOf: [
-	// 							{$ref: refs.BASE},
-	// 							{
-	// 								type: 'object',
-	// 								properties: {
-	// 									error: {
-	// 										type: 'object',
-	// 										properties: {
-	// 											employeeId: {
-	// 												type: 'string',
-	// 												nullable: true
-	// 											},
-	// 											isLeader: {
-	// 												type: 'string',
-	// 												nullable: true
-	// 											}
-	// 										}
-	// 									}
-	// 								}
-	// 							}
-	// 						]
-	// 					}
-	// 				}
-	// 			}
-	// 		},
-	// 		"404": {
-	// 			content: {
-	// 				"application/json": {
-	// 					schema: {
-	// 						$ref: refs.BASE
-	// 					}
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// 	},
-	// 	summary: "Update Project Team Member",
-	// 	tags: [tags.Projects],
-	// },
+	put: {
+		parameters: [
+			{
+				in: 'path',
+				name: 'projectId',
+				required: true,
+				schema: {
+					type: 'string',
+					format: 'uuid'
+				}
+			},
+			{
+				in: 'path',
+				name: 'memberId',
+				required: true,
+				schema: {
+					type: 'string',
+					format: 'uuid'
+				}
+			},
+		],
+		requestBody: {
+			required: true,
+			content: {
+				"application/json": {
+					schema: {
+						type: 'object',
+						properties: {
+							team: {
+								type: 'object',
+								properties: {
+									employeeId: {
+										type: 'string',
+										format: 'uuid'
+									},
+									isLeader: {
+										type: 'boolean'
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		},
+		responses: {
+			responses: {
+			...responses,
+			"200": {
+				content: {
+					"application/json": {
+						schema: {
+							allOf: [
+								{$ref: refs.BASE},
+								{
+									type: 'object',
+									properties: {
+										data: {
+											$ref: refs.PROJECT_TEAM
+										}
+									}
+								}
+							]
+						}
+					}
+				}
+			},
+			"400": {
+				content: {
+					"application/json": {
+						schema: {
+							allOf: [
+								{$ref: refs.BASE},
+								{
+									type: 'object',
+									properties: {
+										error: {
+											type: 'object',
+											properties: {
+												employeeId: {
+													type: 'string',
+													nullable: true
+												},
+												isLeader: {
+													type: 'string',
+													nullable: true
+												}
+											}
+										}
+									}
+								}
+							]
+						}
+					}
+				}
+			},
+			"404": {
+				content: {
+					"application/json": {
+						schema: {
+							$ref: refs.BASE
+						}
+					}
+				}
+			}
+		}
+		},
+		summary: "Update Project Team Member",
+		tags: [tags.Projects],
+	},
 	delete: {
 		parameters: [
 			{
