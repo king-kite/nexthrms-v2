e084bf815bd113ad8b067cfc9f410eeceacf9d64
kingkite <kitewebdevelopers@gmail.com>
2022-12-25 21:21:35 +0100

    Added API Doc for adding project team

--- src/documentation/paths/projects/projectTeam.ts
+++ src/documentation/paths/projects/projectTeam.ts
@@ -69,9 +69,119 @@ const path = {
 						}
 					}
 				}
+			},
+			"404": {
+				content: {
+					"application/json": {
+						schema: {
+							$ref: refs.BASE
+						}
+					}
+				}
 			}
 		},
 		summary: 'Get Project Team',
+		tags: [tags.Projects]
+	},
+	post: {
+		parameters: [
+			{
+				in: 'path',
+				name: 'projectId',
+				required: true,
+				schema: {
+					type: 'string',
+					format: 'uuid'
+				}
+			}
+		],
+		requestBody: {
+			required: true,
+			content: {
+				"application/json": {
+					schema: {
+						type: 'object',
+						properties: {
+							team: {
+								type: 'array',
+								items: {
+									type: 'object',
+									properties: {
+										employeeId: {
+											type: 'string',
+											format: 'uuid'
+										},
+										isLeader: {
+											type: 'boolean'
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		},
+		responses: {
+			...responses,
+			"201": {
+				content: {
+					"application/json": {
+						schema: {
+							allOf: [
+								{$ref: refs.BASE},
+								{
+									type: 'object',
+									properties: {
+										data: {
+											type: 'array',
+											items: {
+												$ref: refs.PROJECT_TEAM
+											}
+										}
+									}
+								}
+							]
+						}
+					}
+				}
+			},
+			"400": {
+				content: {
+					"application/json": {
+						schema: {
+							allOf: [
+								{$ref: refs.BASE},
+								{
+									type: 'object',
+									properties: {
+										error: {
+											type: 'object',
+											properties: {
+												team: {
+													type: 'string',
+													nullable: true
+												}
+											}
+										}
+									}
+								}
+							]
+						}
+					}
+				}
+			},
+			"404": {
+				content: {
+					"application/json": {
+						schema: {
+							$ref: refs.BASE
+						}
+					}
+				}
+			}
+		},
+		summary: "Create Project Team",
 		tags: [tags.Projects]
 	}
 };
