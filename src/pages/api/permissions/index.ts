0b42bd4f16efe4b2c04790736c1040ef801d1f56
kingkite <kitewebdevelopers@gmail.com>
2023-01-03 19:54:11 +0100

    Added a file for fetching and creating permissions

---
+++ src/pages/api/permissions/index.ts
@@ -0,0 +1,39 @@
+import { Prisma } from '@prisma/client';
+
+import { getPermissions, permissionSelectQuery, prisma } from '../../../db';
+import { auth } from '../../../middlewares';
+import { CreatePermissionQueryType } from '../../../types';
+import { createPermissionSchema, validateParams } from '../../../validators';
+
+export default auth()
+	.get(async (req, res) => {
+		const params = validateParams(req.query);
+
+		const data = await getPermissions(params);
+
+		return res.status(200).json({
+			status: 'success',
+			message: 'Fetched permissions successfully! A total of ' + data.total,
+			data,
+		});
+	})
+	.post(async (req, res) => {
+		const data: CreatePermissionQueryType =
+			await createPermissionSchema.validateAsync(
+				{ ...req.body },
+				{
+					abortEarly: false,
+				}
+			);
+
+		const permission = await prisma.permission.create({
+			data,
+			select: permissionSelectQuery,
+		});
+
+		return res.status(201).json({
+			status: 'success',
+			message: 'Permission added successfully',
+			data: permission,
+		});
+	});
