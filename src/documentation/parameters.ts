455236248cbff2334e6ef56a42d56e13c02b88f4
Emmanuel <emmanuel.kolade1@gmail.com>
2023-01-19 21:48:37 +0000

    Added a default parameters file for the documentation

---
+++ src/documentation/parameters.ts
@@ -0,0 +1,53 @@
+const parameters: {
+  in: string;
+  name: string;
+  required?: boolean;
+  schema: {
+    type: string;
+    default?: any;
+    format?: any;
+  }
+}[] = [
+	{
+		in: 'query',
+		name: 'limit',
+		schema: {
+			type: 'number',
+			default: 10,
+		},
+	},
+	{
+		in: 'query',
+		name: 'offset',
+		schema: {
+			type: 'number',
+			default: 0,
+		},
+	},
+	{
+		in: 'query',
+		name: 'from',
+		schema: {
+			type: 'string',
+			format: 'date-time',
+		},
+	},
+	{
+		in: 'query',
+		name: 'to',
+		schema: {
+			type: 'string',
+			format: 'date-time',
+		},
+	},
+	{
+		in: 'query',
+		name: 'search',
+		required: false,
+		schema: {
+			type: 'string',
+		},
+	},
+];
+
+export default parameters;
