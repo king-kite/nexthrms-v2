9958485ccbd64b863c3b236833f5e9965abba076
kingkite <kitewebdevelopers@gmail.com>
2023-02-06 22:03:54 +0100

    Made validate params always return undefined param values if not valid

--- src/validators/pagination.ts
+++ src/validators/pagination.ts
@@ -1,19 +1,21 @@
 import type { NextApiRequest } from 'next';
 
-import { DEFAULT_PAGINATION_SIZE } from '../config/settings';
+// import { DEFAULT_PAGINATION_SIZE } from '../config/settings';
 import { ParamsType } from '../types';
 
 export function validateParams(query: NextApiRequest['query']): ParamsType {
 	const limit =
 		query.limit && !isNaN(Number(query.limit))
 			? Number(query.limit)
-			: DEFAULT_PAGINATION_SIZE;
+			: // : DEFAULT_PAGINATION_SIZE;
+			  undefined;
 	const offset =
 		query.offset && !isNaN(Number(query.offset)) ? Number(query.offset) : 0;
 	const search =
 		typeof query.search === 'string' && query.search.trim() !== ''
 			? query.search
-			: '';
+			: // : '';
+			  undefined;
 	const from =
 		query.from && (typeof query.from === 'string' || query.from instanceof Date)
 			? new Date(query.from)
@@ -21,7 +23,8 @@ export function validateParams(query: NextApiRequest['query']): ParamsType {
 	const to =
 		query.to && (typeof query.to === 'string' || query.to instanceof Date)
 			? new Date(query.to)
-			: new Date();
+			: // : new Date();
+			  undefined;
 
 	return { limit, offset, search, from, to };
 }
