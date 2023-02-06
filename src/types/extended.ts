a6816355c61efe9fd13172731e397b23abfdbd9b
kingkite <kitewebdevelopers@gmail.com>
2023-02-06 16:45:31 +0100

    Fixed Typescript Build Error

--- src/types/extended.ts
+++ src/types/extended.ts
@@ -25,7 +25,7 @@ export type RequestUserType = {
 			name: string;
 		} | null;
 	} | null;
-	groups: GroupType[];
+	groups: Omit<GroupType, 'users'>[];
 	permissions: PermissionType[];
 	checkPassword: (password: string) => Promise<boolean>;
 };
