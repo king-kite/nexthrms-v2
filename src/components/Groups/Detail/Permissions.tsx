18209bb393fd6a414fe1a412899f00da7e0c932c
kingkite <kitewebdevelopers@gmail.com>
2023-02-07 21:54:05 +0100

    Sorted the permission in the group page info

--- src/components/Groups/Detail/Permissions.tsx
+++ src/components/Groups/Detail/Permissions.tsx
@@ -13,6 +13,34 @@ function Permissions({
 }) {
 	const { open: openModal, close, showLoader } = useAlertModalContext();
 
+	const sortedPermissions = React.useMemo(() => {
+		// Get the names of all permission categories and sort it;
+		let categoryNames: string[] = [];
+		permissions.forEach((permission) => {
+			const categoryName = permission.category
+				? permission.category.name.toLowerCase()
+				: 'anonymous';
+			if (!categoryNames.includes(categoryName.toLowerCase())) {
+				categoryNames.push(categoryName.toLowerCase());
+			}
+		});
+		categoryNames = categoryNames.sort();
+
+		// Sort permissions according to their category if they have one!
+		const sortedPermissions = categoryNames.map((category) => {
+			const categoryPermissions = permissions.filter((permission) => {
+				if (!permission.category && category === 'anonymous') return permission;
+				return (
+					permission.category &&
+					permission.category.name.toLowerCase() === category
+				);
+			});
+			return { category, permissions: categoryPermissions };
+		}, []);
+
+		return sortedPermissions;
+	}, [permissions]);
+
 	const removeSinglePermission = React.useCallback(
 		(codename: string) => {
 			openModal({
@@ -43,28 +71,38 @@ function Permissions({
 	);
 
 	return (
-		<div className="gap-4 grid grid-cols-1 md:gap-6 md:grid-cols-2">
-			{permissions.map((permission, index) => (
-				<div
-					className="bg-gray-200 border border-gray-400 border-l-8 p-2 rounded-md lg:pl-4 lg:p-3"
-					key={index}
-				>
-					<div className="flex items-start justify-between py-1 w-full">
-						<h4 className="capitalize font-bold text-base text-gray-800 w-full md:text-lg">
-							{permission.name.toLowerCase()}
-						</h4>
-						<div
-							onClick={() => removeSinglePermission(permission.codename)}
-							className="cursor-pointer duration-500 mx-4 p-2 rounded-full text-gray-700 text-xs transform transition-all hover:bg-white hover:scale-110 hover:text-gray-600 md:text-sm"
-						>
-							<FaTimes className="text-xs sm:text-sm" />
-						</div>
+		<div>
+			{sortedPermissions.map(({ category, permissions }, index) => (
+				<div key={index} className="mb-5">
+					<h4 className="capitalize font-bold my-3 text-base text-primary-600 w-full md:text-lg">
+						{category}
+					</h4>
+					<div className="gap-4 grid grid-cols-1 md:gap-6 md:grid-cols-2">
+						{permissions.map((permission, index) => (
+							<div
+								className="bg-gray-200 border border-gray-400 border-l-8 p-2 rounded-md lg:pl-4 lg:p-3"
+								key={index}
+							>
+								<div className="flex items-center justify-between py-1 w-full">
+									<h4 className="capitalize font-bold text-base text-gray-800 w-full md:text-lg">
+										{permission.name.toLowerCase()}
+									</h4>
+									<div
+										onClick={() => removeSinglePermission(permission.codename)}
+										className="cursor-pointer duration-500 mx-4 p-2 rounded-full text-gray-700 text-xs transform transition-all hover:bg-white hover:scale-110 hover:text-gray-600 md:text-sm"
+									>
+										<FaTimes className="text-xs sm:text-sm" />
+									</div>
+								</div>
+								{permission.description && (
+									<p className="font-medium my-1 pr-2 text-gray-700 text-sm md:text-base">
+										{permission.description}
+									</p>
+								)}
+							</div>
+						))}
 					</div>
-					{permission.description && (
-						<p className="font-medium my-1 pr-2 text-gray-700 text-sm md:text-base">
-							{permission.description}
-						</p>
-					)}
+					{index + 1 !== sortedPermissions.length && <hr className="mt-5" />}
 				</div>
 			))}
 		</div>
