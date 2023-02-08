4e85d57773ef654663b62ecbb7570a37bcf1c7b1
kingkite <kitewebdevelopers@gmail.com>
2023-02-08 19:55:46 +0100

    added the user permission route to the documentation

--- src/documentation/paths/users/index.ts
+++ src/documentation/paths/users/index.ts
@@ -1,17 +1,19 @@
-import * as routes from "../../../config/server";
+import * as routes from '../../../config/server';
 
-import exportUsers from "./exportUsers";
-import userActivation from "./userActivation";
-import passwordChange from "./passwordChange";
-import user from "./user"; // Single User Path
-import users from "./users";
+import exportUsers from './exportUsers';
+import userActivation from './userActivation';
+import passwordChange from './passwordChange';
+import user from './user'; // Single User Path
+import users from './users';
+import userPermissions from './userPermissions';
 
 const paths = {
 	[routes.USERS_URL]: users,
-	[routes.USER_URL("{id}")]: user,
+	[routes.USER_URL('{id}')]: user,
+	[routes.USER_PERMISSIONS_URL('{id}')]: userPermissions,
 	[routes.CHANGE_USER_PASSWORD_URL]: passwordChange,
 	[routes.ACTIVATE_USER_URL]: userActivation,
-	[routes.USERS_EXPORT_URL]: exportUsers
-}
+	[routes.USERS_EXPORT_URL]: exportUsers,
+};
 
 export default paths;
