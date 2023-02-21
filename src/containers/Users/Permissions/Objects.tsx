44d8a84e33d0d24e0a25db69476f017b7c4464ce
kingkite <kitewebdevelopers@gmail.com>
2023-02-21 17:08:29 +0100

    Adding object permissions page

--- src/containers/Users/Permissions/Objects.tsx
+++ src/containers/Users/Permissions/Objects.tsx
@@ -6,6 +6,8 @@ import { Container } from '../../../components/common';
 import { DEFAULT_PAGINATION_SIZE } from '../../../config';
 import { useGetObjectPermissionsQuery } from '../../../store/queries';
 import {
+	ObjectPermissionType,
+	ObjectPermissionUserType,
 	GetObjectPermissionsResponseType,
 	PermissionModelNameType,
 } from '../../../types';
@@ -45,10 +47,45 @@ function ObjectPermissions({
 		}
 	);
 
+	const users = React.useMemo(() => {
+		if (!data) return [];
+
+		// scoped users array with will include boolean fields for permissions
+		const users: (ObjectPermissionUserType & {
+			permissions: ('CREATE' | 'DELETE' | 'EDIT' | 'VIEW')[];
+		})[] = [];
+
+		data.result.forEach((objPerm) => {
+			// loop through the users in the objPerm and
+			// check if there is a user already in the scoped user variable
+			objPerm.users.forEach((user) => {
+				const found = users.find((item) => item.id === user.id);
+				// Check if the user exists in the scoped variable
+				// and does not have this objPerm permission yet
+				if (found && !found.permissions.includes(objPerm.permission)) {
+					const index = users.indexOf(found);
+					users[index] = {
+						...found,
+						permissions: [...found.permissions, objPerm.permission],
+					};
+				} else {
+					users.push({
+						...user,
+						permissions: [objPerm.permission],
+					});
+				}
+			});
+		});
+
+		return users;
+	}, [data]);
+
+	console.log({ users });
+
 	return (
 		<Container
 			heading="Object/Record Permissions"
-      icon
+			icon
 			refresh={{
 				loading: isFetching,
 				onClick: refetch,
