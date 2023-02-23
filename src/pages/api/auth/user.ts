3c1cf3ceec0850a9877fa0074a43d96f058137ec
kingkite <kitewebdevelopers@gmail.com>
2023-02-23 11:43:50 +0100

    the auth user api route now integrates serializeUserData Function

--- src/pages/api/auth/user.ts
+++ src/pages/api/auth/user.ts
@@ -1,34 +1,8 @@
 import { auth } from '../../../middlewares';
-import { AuthDataType } from '../../../types';
+import { serializeUserData } from '../../../utils/serializers';
 
 export default auth().get((req, res) => {
-	const data: AuthDataType = {
-		firstName: req.user.firstName,
-		lastName: req.user.lastName,
-		fullName: req.user.fullName,
-		email: req.user.email,
-		profile: req.user.profile
-			? {
-					image: req.user.profile.image,
-			  }
-			: null,
-		employee: null,
-		permissions: req.user.permissions
-	};
-	if (req.user.employee) {
-		data.employee = {
-			id: req.user.employee.id,
-		};
-		if (req.user.employee.job) {
-			data.employee.job = {
-				name: req.user.employee.job.name,
-			};
-		}
-	}
-
-	if (req.user.isAdmin) data.isAdmin = true;
-
-	if (req.user.isSuperUser) data.isSuperUser = true;
+	const data = serializeUserData(req.user);
 
 	return res.status(200).json({
 		message: 'Verified successfully',
