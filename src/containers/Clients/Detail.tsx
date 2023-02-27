c7d2fef61f14b2baabb27e5c739dfc19c3c933ca
kingkite <kitewebdevelopers@gmail.com>
2023-02-27 16:14:53 +0100

    Added edit, delete obj perms in detail

--- src/containers/Clients/Detail.tsx
+++ src/containers/Clients/Detail.tsx
@@ -67,10 +67,18 @@ const ClientDetail = ({ client }: { client: ClientType }) => {
 		const buttons: ButtonType[] = [];
 		const canEdit =
 			authData.isSuperUser ||
-			hasModelPermission(authData.permissions, [permissions.client.EDIT]);
+			hasModelPermission(authData.permissions, [permissions.client.EDIT]) ||
+			// check object permission
+			!!authData?.objPermissions.find(
+				(perm) => perm.modelName === 'clients' && perm.permission === 'EDIT'
+			);
 		const canDelete =
 			authData.isSuperUser ||
-			hasModelPermission(authData.permissions, [permissions.client.DELETE]);
+			hasModelPermission(authData.permissions, [permissions.client.DELETE]) ||
+			// check object permission
+			!!authData?.objPermissions.find(
+				(perm) => perm.modelName === 'clients' && perm.permission === 'DELETE'
+			);
 		const canViewObjectPermissions =
 			authData.isSuperUser ||
 			hasModelPermission(authData.permissions, [
