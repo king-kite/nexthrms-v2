b779870f967ea1edcc57365672475dd8f6afc08e
kingkite <kitewebdevelopers@gmail.com>
2023-03-01 22:40:24 +0100

    Bug Fix

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -4,7 +4,7 @@ export function getStringedDate(_date?: Date | string) {
 			? new Date(_date)
 			: _date
 		: new Date();
-	let day = date.getDate();
+	let day: string | number = date.getDate();
 	if (day < 10) day = `0${day}`;
 	let month: string | number = date.getMonth() + 1; // month starts from 0 to 11
 	if (month < 10) month = `0${month}`;
