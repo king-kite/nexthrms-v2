ac81feee67ffc4d77636c441db174e7299bce69b
kingkite <kitewebdevelopers@gmail.com>
2023-03-05 21:15:42 +0100

    Added user permissions to the user's permissions and groups page

--- src/pages/api/users/[id]/permissions.ts
+++ src/pages/api/users/[id]/permissions.ts
@@ -1,12 +1,33 @@
+import { permissions } from '../../../../config';
 import { getUserPermissions, prisma } from '../../../../db';
-import { auth } from '../../../../middlewares';
+import { getUserObjectPermissions } from '../../../db/utils';
+import { admin } from '../../../../middlewares';
+import { hasModelPermission } from '../../../../utils';
+import { NextApiErrorMessage } from '../../../../utils/classes';
 import {
 	updateUserPermissionsSchema,
 	validateParams,
 } from '../../../../validators';
 
-export default auth()
+export default admin()
 	.get(async (req, res) => {
+		let hasPerm =
+			req.user.isSuperUser ||
+			hasModelPermission(req.user.allPermissions, [permissions.user.VIEW]);
+
+		if (!hasPerm) {
+			// check if the user has a view object permission for this record
+			const objPerm = await getUserObjectPermissions({
+				modelName: 'users',
+				objectId: req.query.id as string,
+				permission: 'VIEW',
+				userId: req.user.id,
+			});
+			if (objPerm.view === true) hasPerm = true;
+		}
+
+		if (!hasPerm) throw new NextApiErrorMessage(403);
+
 		const params = validateParams(req.query);
 		const data = await getUserPermissions(req.query.id as string, params);
 
@@ -18,6 +39,23 @@ export default auth()
 		});
 	})
 	.put(async (req, res) => {
+		let hasPerm =
+			req.user.isSuperUser ||
+			hasModelPermission(req.user.allPermissions, [permissions.user.EDIT]);
+
+		if (!hasPerm) {
+			// check if the user has a view object permission for this record
+			const objPerm = await getUserObjectPermissions({
+				modelName: 'users',
+				objectId: req.query.id as string,
+				permission: 'EDIT',
+				userId: req.user.id,
+			});
+			if (objPerm.edit === true) hasPerm = true;
+		}
+
+		if (!hasPerm) throw new NextApiErrorMessage(403);
+
 		const data: {
 			permissions: string[];
 		} = await updateUserPermissionsSchema.validateAsync({ ...req.body });
