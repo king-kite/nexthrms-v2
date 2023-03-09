648621d12023e505a27c076c04a32310785de250
kingkite <kitewebdevelopers@gmail.com>
2023-03-09 09:42:45 +0100

    Added permissions to the Permission page

--- src/pages/users/permissions/index.tsx
+++ src/pages/users/permissions/index.tsx
@@ -1,17 +1,22 @@
+import type { InferGetServerSidePropsType } from 'next';
 import React from 'react';
 
-import { LOGIN_PAGE_URL } from '../../../config';
+import {
+	permissions,
+	DEFAULT_PAGINATION_SIZE,
+	LOGIN_PAGE_URL,
+} from '../../../config';
 import Permissions from '../../../containers/Users/Permissions';
 import { getPermissions } from '../../../db';
+import { getUserObjects } from '../../../db/utils';
 import { authPage } from '../../../middlewares';
-import {
-	ExtendedGetServerSideProps,
-	GetPermissionsResponseType,
-} from '../../../types';
-import { Title } from '../../../utils';
+import { ExtendedGetServerSideProps } from '../../../types';
+import { hasModelPermission, Title } from '../../../utils';
 import { serializeUserData } from '../../../utils/serializers';
 
-const Page = ({ data }: { data: GetPermissionsResponseType['data'] }) => (
+const Page = ({
+	data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => (
 	<React.Fragment>
 		<Title title="Permissions" />
 		<Permissions permissions={data} />
@@ -39,12 +44,71 @@ export const getServerSideProps: ExtendedGetServerSideProps = async ({
 	}
 
 	const auth = await serializeUserData(req.user);
-	const data = await getPermissions();
+
+	// Check is admin
+	if (!req.user.isAdmin && !req.user.isSuperUser)
+		return {
+			props: {
+				auth,
+				errorPage: {
+					statusCode: 403,
+				},
+			},
+		};
+
+	const hasViewPerm =
+		req.user.isSuperUser ||
+		hasModelPermission(req.user.allPermissions, [permissions.permission.VIEW]);
+
+	if (hasViewPerm) {
+		const data = await getPermissions({
+			limit: DEFAULT_PAGINATION_SIZE,
+			offset: 0,
+			search: '',
+		});
+
+		return {
+			props: {
+				auth,
+				data: JSON.parse(JSON.stringify(data)),
+			},
+		};
+	}
+
+	// Since the user is not a super user and doe not have model permissions
+	// check if the user has a view object permission for any record in this table
+	const records = await getUserObjects({
+		modelName: 'permissions',
+		permission: 'VIEW',
+		userId: req.user.id,
+	});
+	if (records.length > 0) {
+		const data = await getPermissions({
+			limit: DEFAULT_PAGINATION_SIZE,
+			offset: 0,
+			search: '',
+			where: {
+				id: {
+					in: records.map((obj) => obj.objectId),
+				},
+			},
+		});
+		if (data.total > 0) {
+			return {
+				props: {
+					auth,
+					data: JSON.parse(JSON.stringify(data)),
+				},
+			};
+		}
+	}
 
 	return {
 		props: {
 			auth,
-			data: JSON.parse(JSON.stringify(data)),
+			errorPage: {
+				statusCode: 403,
+			},
 		},
 	};
 };
