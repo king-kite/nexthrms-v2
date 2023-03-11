b1207e8fd50c6ef4e00c52b1bd0525e152cdbcb6
kingkite <kitewebdevelopers@gmail.com>
2023-03-11 12:15:40 +0100

    Added permissions for the group lists page

--- src/pages/users/groups/index.tsx
+++ src/pages/users/groups/index.tsx
@@ -1,17 +1,22 @@
+import type { InferGetServerSidePropsType } from 'next';
 import React from 'react';
 
-import { DEFAULT_PAGINATION_SIZE, LOGIN_PAGE_URL } from '../../../config';
+import {
+	permissions,
+	DEFAULT_PAGINATION_SIZE,
+	LOGIN_PAGE_URL,
+} from '../../../config';
 import Groups from '../../../containers/Users/Groups';
 import { getGroups } from '../../../db';
+import { getUserObjects } from '../../../db/utils';
 import { authPage } from '../../../middlewares';
-import {
-	ExtendedGetServerSideProps,
-	GetGroupsResponseType,
-} from '../../../types';
-import { Title } from '../../../utils';
+import { ExtendedGetServerSideProps } from '../../../types';
+import { hasModelPermission, Title } from '../../../utils';
 import { serializeUserData } from '../../../utils/serializers';
 
-const Page = ({ data }: { data: GetGroupsResponseType['data'] }) => (
+const Page = ({
+	data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => (
 	<React.Fragment>
 		<Title title="Groups" />
 		<Groups groups={data} />
@@ -39,7 +44,23 @@ export const getServerSideProps: ExtendedGetServerSideProps = async ({
 	}
 
 	const auth = await serializeUserData(req.user);
-	const data = await getGroups({
+
+	// Check is admin
+	if (!req.user.isAdmin && !req.user.isSuperUser)
+		return {
+			props: {
+				auth,
+				errorPage: {
+					statusCode: 403,
+				},
+			},
+		};
+
+	const hasViewPerm =
+		req.user.isSuperUser ||
+		hasModelPermission(req.user.allPermissions, [permissions.group.VIEW]);
+
+	const params = {
 		limit: DEFAULT_PAGINATION_SIZE,
 		offset: 0,
 		search: '',
@@ -48,12 +69,66 @@ export const getServerSideProps: ExtendedGetServerSideProps = async ({
 			offset: 0,
 			search: '',
 		},
+	};
+
+	if (hasViewPerm) {
+		const data = await getGroups(params);
+
+		return {
+			props: {
+				auth,
+				data: JSON.parse(JSON.stringify(data)),
+			},
+		};
+	}
+
+	// Since the user is not a super user and doe not have model permissions
+	// check if the user has a view object permission for any record in this table
+	const records = await getUserObjects({
+		modelName: 'groups',
+		permission: 'VIEW',
+		userId: req.user.id,
 	});
+	if (records.length > 0) {
+		const data = await getGroups({
+			...params,
+			where: {
+				id: {
+					in: records.map((obj) => obj.objectId),
+				},
+			},
+		});
+		if (data.total > 0) {
+			return {
+				props: {
+					auth,
+					data: JSON.parse(JSON.stringify(data)),
+				},
+			};
+		}
+	}
+
+	// If the user does not have model level and object level permission
+	// check if the user has CREATE permission
+	const hasCreatePerm = hasModelPermission(req.user.allPermissions, [
+		permissions.group.CREATE,
+	]);
+
+	if (hasCreatePerm) {
+		return {
+			props: {
+				auth,
+				data: undefined,
+			},
+		};
+	}
 
 	return {
 		props: {
 			auth,
-			data: JSON.parse(JSON.stringify(data)),
+			errorPage: {
+				statusCode: 403,
+			},
 		},
 	};
 };
