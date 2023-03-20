51f2b24bc97d3d05f9816639257fae7db96755b6
kingkite <kitewebdevelopers@gmail.com>
2023-03-20 22:16:33 +0100

    Added function to get an employee admin officers

--- src/db/utils/permission.ts
+++ src/db/utils/permission.ts
@@ -264,66 +264,66 @@ export async function removeObjectPermissions({
 		);
 }
 
-// export async function updateObjectPermissions({
-// 	model: modelName,
-// 	permissions = ['DELETE', 'EDIT', 'VIEW'],
-// 	objectId,
-// 	users,
-// }: {
-// 	model: PermissionModelChoices;
-// 	permissions?: PermissionObjectChoices[];
-// 	objectId: string;
-// 	users: string[];
-// }) {
-// 	return prisma.$transaction(
-// 		permissions.map((permission) =>
-// 			prisma.permissionObject.update({
-// 				data: {
-// 					users: {
-// 						connect: users.map((user) => ({ id: user })),
-// 					},
-// 				},
-// 				where: {
-// 					modelName_objectId_permission: {
-// 						modelName,
-// 						permission,
-// 						objectId,
-// 					},
-// 				},
-// 				select: { id: true },
-// 			})
-// 		)
-// 	);
-// }
+export async function getEmployeeOfficersId(id: string) {
+	// simply get the employee and get he's supervisors and hod
+	// get the superusers will be admins
+	const [employee, admins] = await prisma.$transaction([
+		prisma.employee.findUnique({
+			where: { id },
+			select: {
+				department: {
+					select: {
+						hod: {
+							select: {
+								user: {
+									select: {
+										id: true,
+										isActive: true,
+									},
+								},
+							},
+						},
+					},
+				},
+				supervisors: {
+					where: {
+						user: {
+							isActive: true,
+							isAdmin: true,
+						},
+					},
+					select: {
+						user: {
+							select: {
+								id: true,
+							},
+						},
+					},
+				},
+			},
+		}),
+		prisma.user.findMany({
+			where: {
+				isActive: true,
+				isSuperUser: true,
+			},
+			select: { id: true },
+		}),
+	]);
+	const officers = admins.map((admin) => admin.id);
+	if (employee) {
+		if (
+			employee.department?.hod &&
+			employee.department.hod.user.isActive &&
+			!officers.includes(employee.department.hod.user.id)
+		)
+			officers.push(employee.department.hod.user.id);
 
-// export async function removeObjectPermissions({
-// 	model: modelName,
-// 	permissions = ['DELETE', 'EDIT', 'VIEW'],
-// 	objectId,
-// 	users,
-// }: {
-// 	model: PermissionModelChoices;
-// 	permissions?: PermissionObjectChoices[];
-// 	objectId: string;
-// 	users: string[];
-// }) {
-// 	return prisma.$transaction(
-// 		permissions.map((permission) =>
-// 			prisma.permissionObject.update({
-// 				data: {
-// 					users: {
-// 						disconnect: users.map((user) => ({ id: user })),
-// 					},
-// 				},
-// 				where: {
-// 					modelName_objectId_permission: {
-// 						modelName,
-// 						permission,
-// 						objectId,
-// 					},
-// 				},
-// 				select: { id: true },
-// 			})
-// 		)
-// 	);
-// }
+		employee.supervisors.forEach((supervisor) => {
+			if (!officers.includes(supervisor.user.id))
+				officers.push(supervisor.user.id);
+		});
+	}
+
+	return officers;
+}
