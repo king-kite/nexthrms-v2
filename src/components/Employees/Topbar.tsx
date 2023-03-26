568a8ab8733f8ef5870a06b9820a2e2a4bd66d5a
kingkite <kitewebdevelopers@gmail.com>
2023-03-26 12:16:54 +0100

    Removed unnecessary variables

--- src/components/Employees/Topbar.tsx
+++ src/components/Employees/Topbar.tsx
@@ -25,18 +25,7 @@ const Topbar: FC<TopbarProps> = ({
 	const searchRef = useRef<HTMLInputElement | null>(null);
 	const { data: authData } = useAuthContext();
 
-	const [canView, canCreate, canExport] = useMemo(() => {
-		const canView = authData
-			? authData.isSuperUser ||
-			  (authData.isAdmin &&
-					hasModelPermission(authData.permissions, [
-						permissions.employee.VIEW,
-					])) ||
-			  // check object permission
-			  !!authData?.objPermissions.find(
-					(perm) => perm.modelName === 'employees' && perm.permission === 'VIEW'
-			  )
-			: false;
+	const [canCreate, canExport] = useMemo(() => {
 		const canCreate = authData
 			? authData.isSuperUser ||
 			  (authData.isAdmin &&
@@ -51,44 +40,41 @@ const Topbar: FC<TopbarProps> = ({
 						permissions.employee.EXPORT,
 					]))
 			: false;
-		return [canView, canCreate, canExport];
+		return [canCreate, canExport];
 	}, [authData]);
 
 	return (
 		<div className="flex flex-col my-2 w-full lg:flex-row lg:items-center">
-			{canView && (
-				<form
-					className="flex items-center mb-3 pr-8 w-full lg:mb-0 lg:w-3/5"
-					onSubmit={(e) => {
-						e.preventDefault();
-						if (searchRef.current) onSubmit(searchRef.current.value);
+			<form
+				className="flex items-center mb-3 pr-8 w-full lg:mb-0 lg:w-3/5"
+				onSubmit={(e) => {
+					e.preventDefault();
+					if (searchRef.current) onSubmit(searchRef.current.value);
+				}}
+			>
+				<InputButton
+					buttonProps={{
+						caps: true,
+						disabled: loading,
+						iconLeft: FaSearch,
+						// padding: 'pl-2 pr-4 py-[0.54rem]',
+						title: 'Search',
+						type: 'submit',
 					}}
-				>
-					<InputButton
-						buttonProps={{
-							caps: true,
-							disabled: loading,
-							iconLeft: FaSearch,
-							// padding: 'pl-2 pr-4 py-[0.54rem]',
-							title: 'Search',
-							type: 'submit',
-						}}
-						inputProps={{
-							// bdrColor: 'border-primary-500',
-							disabled: loading,
-							icon: FaSearch,
-							onChange: ({ target: { value } }) => {
-								if (!value || value === '') onSubmit('');
-							},
-							placeholder: "Search employee's name, or e-mail",
-							rounded: 'rounded-l-lg',
-							type: 'search',
-						}}
-						ref={searchRef}
-					/>
-				</form>
-			)}
-
+					inputProps={{
+						// bdrColor: 'border-primary-500',
+						disabled: loading,
+						icon: FaSearch,
+						onChange: ({ target: { value } }) => {
+							if (!value || value === '') onSubmit('');
+						},
+						placeholder: "Search employee's name, or e-mail",
+						rounded: 'rounded-l-lg',
+						type: 'search',
+					}}
+					ref={searchRef}
+				/>
+			</form>
 			{canExport && (
 				<div className="my-3 pr-4 w-full sm:w-1/3 lg:my-0 lg:px-4 xl:px-5 xl:w-1/4">
 					<ButtonDropdown
