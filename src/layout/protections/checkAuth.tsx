693033fe57c320a506f1f0dc483dbda3e227c0e1
kingkite <kitewebdevelopers@gmail.com>
2023-04-10 12:06:49 +0100

    No longer using react query to login user

--- src/layout/protections/checkAuth.tsx
+++ src/layout/protections/checkAuth.tsx
@@ -1,4 +1,3 @@
-import { useQuery } from '@tanstack/react-query';
 import axios, { AxiosResponse } from 'axios';
 import React from 'react';
 
@@ -14,31 +13,36 @@ const CheckAuth = ({
 	children: React.ReactNode;
 	authData?: AuthDataType;
 }) => {
+	const [loading, setLoading] = React.useState(!authData ? true : false);
+
 	const { login, logout } = useAuthContext();
 
-	const { isLoading } = useQuery(
-		['auth'],
-		() =>
-			axios
-				.get(USER_DATA_URL)
-				.then(
-					(response: AxiosResponse<SuccessResponseType<AuthDataType>>) =>
-						response.data.data
-				),
-		{
-			onSuccess(data: AuthDataType) {
-				login(data);
-			},
-			initialData() {
-				return authData;
-			},
-			onError() {
-				logout();
-			},
+	React.useEffect(() => {
+		if (authData) {
+			setLoading(false);
+			login(authData);
 		}
-	);
+	}, [authData, login]);
 
-	return isLoading ? (
+	React.useEffect(() => {
+		async function checkAuth() {
+			if (!authData) setLoading(true);
+			await axios
+				.get(USER_DATA_URL)
+				.then((response: AxiosResponse<SuccessResponseType<AuthDataType>>) =>
+					login(response.data.data)
+				)
+				.catch(() => {
+					logout();
+				})
+				.finally(() => {
+					setLoading(false);
+				});
+		}
+		checkAuth();
+	}, [login, logout, authData]);
+
+	return loading ? (
 		<SplashScreen />
 	) : (
 		<React.Fragment>{children}</React.Fragment>
@@ -46,3 +50,52 @@ const CheckAuth = ({
 };
 
 export default CheckAuth;
+
+// import { useQuery } from '@tanstack/react-query';
+// import axios, { AxiosResponse } from 'axios';
+// import React from 'react';
+
+// import { SplashScreen } from '../../components/common';
+// import { USER_DATA_URL } from '../../config';
+// import { useAuthContext } from '../../store/contexts';
+// import { SuccessResponseType, AuthDataType } from '../../types';
+
+// const CheckAuth = ({
+// 	children,
+// 	authData,
+// }: {
+// 	children: React.ReactNode;
+// 	authData?: AuthDataType;
+// }) => {
+// 	const { login, logout } = useAuthContext();
+
+// 	const { isLoading } = useQuery(
+// 		['auth'],
+// 		() =>
+// 			axios
+// 				.get(USER_DATA_URL)
+// 				.then(
+// 					(response: AxiosResponse<SuccessResponseType<AuthDataType>>) =>
+// 						response.data.data
+// 				),
+// 		{
+// 			onSuccess(data: AuthDataType) {
+// 				login(data);
+// 			},
+// 			initialData() {
+// 				return authData;
+// 			},
+// 			onError() {
+// 				logout();
+// 			},
+// 		}
+// 	);
+
+// 	return isLoading ? (
+// 		<SplashScreen />
+// 	) : (
+// 		<React.Fragment>{children}</React.Fragment>
+// 	);
+// };
+
+// export default CheckAuth;
