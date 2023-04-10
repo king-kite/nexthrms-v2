d0c456a8396a327117a5b9cab4f0e37b355c8cff
kingkite <kitewebdevelopers@gmail.com>
2023-04-10 10:17:59 +0100

    Added select user to the employee form

--- src/components/Employees/Form.tsx
+++ src/components/Employees/Form.tsx
@@ -7,13 +7,14 @@ import {
 	Select2,
 	Textarea,
 } from 'kite-react-tailwind';
-import { FC, useCallback, useEffect, useRef, useState } from 'react';
+import React from 'react';
 
 import { DEFAULT_PAGINATION_SIZE, DEFAULT_IMAGE } from '../../config';
 import {
 	useGetDepartmentsQuery,
 	useGetEmployeesQuery,
 	useGetJobsQuery,
+	useGetUsersQuery,
 } from '../../store/queries';
 
 import {
@@ -50,7 +51,7 @@ function handleDataError(err: unknown): string | undefined {
 	return undefined;
 }
 
-const Form: FC<FormProps> = ({
+const Form = ({
 	editMode,
 	initState,
 	errors,
@@ -58,23 +59,27 @@ const Form: FC<FormProps> = ({
 	loading,
 	success,
 	onSubmit,
-}) => {
-	const [depLimit, setDepLimit] = useState(DEFAULT_PAGINATION_SIZE);
-	const [empLimit, setEmpLimit] = useState(DEFAULT_PAGINATION_SIZE);
-	const [jobLimit, setJobLimit] = useState(DEFAULT_PAGINATION_SIZE);
+}: FormProps) => {
+	const [selectUser, setSelectUser] = React.useState(false);
+
+	const [depLimit, setDepLimit] = React.useState(DEFAULT_PAGINATION_SIZE);
+	const [empLimit, setEmpLimit] = React.useState(DEFAULT_PAGINATION_SIZE);
+	const [jobLimit, setJobLimit] = React.useState(DEFAULT_PAGINATION_SIZE);
+	const [usrLimit, setUsrLimit] = React.useState(DEFAULT_PAGINATION_SIZE);
 
 	// defaultValue doesn't re-render after these department, employees, and jobs
 	// are loading. Use state instead
-	const [form, setForm] = useState({
-		image: '',
+	const [form, setForm] = React.useState({
 		department: initState?.department?.id || '',
+		image: '',
 		job: initState?.job?.id || '',
 		supervisors:
 			initState?.supervisors.map((supervisor) => supervisor.id) || [],
+		userId: initState?.user.id || null,
 	});
-	const [formErrors, setErrors] = useState<ErrorType>();
+	const [formErrors, setErrors] = React.useState<ErrorType>();
 
-	const formRef = useRef<HTMLFormElement | null>(null);
+	const formRef = React.useRef<HTMLFormElement | null>(null);
 
 	const jobs = useGetJobsQuery({ limit: jobLimit, offset: 0, search: '' });
 	const employees = useGetEmployeesQuery({
@@ -87,12 +92,18 @@ const Form: FC<FormProps> = ({
 		offset: 0,
 		search: '',
 	});
+	const users = useGetUsersQuery({
+		limit: usrLimit,
+		offset: 0,
+		search: '',
+	});
 
 	const departmentsError = handleDataError(departments.error);
 	const employeesError = handleDataError(employees.error);
 	const jobsError = handleDataError(jobs.error);
+	const usersError = handleDataError(users.error);
 
-	const handleSubmit = useCallback(
+	const handleSubmit = React.useCallback(
 		async (input: CreateEmployeeQueryType) => {
 			try {
 				const valid: CreateEmployeeQueryType =
@@ -122,7 +133,7 @@ const Form: FC<FormProps> = ({
 		[onSubmit]
 	);
 
-	const removeFormErrors = useCallback(
+	const removeFormErrors = React.useCallback(
 		(name: string) => {
 			if (Object(formErrors)[name]) {
 				setErrors((prevState) => ({
@@ -134,7 +145,7 @@ const Form: FC<FormProps> = ({
 		[formErrors]
 	);
 
-	const handleFormChange = useCallback(
+	const handleFormChange = React.useCallback(
 		(name: string, value: string | string[]) => {
 			setForm((prevState) => ({
 				...prevState,
@@ -145,9 +156,15 @@ const Form: FC<FormProps> = ({
 		[removeFormErrors]
 	);
 
-	useEffect(() => {
+	React.useEffect(() => {
 		if (success && !editMode) {
-			setForm({ image: '', job: '', department: '', supervisors: [] });
+			setForm({
+				image: '',
+				job: '',
+				department: '',
+				supervisors: [],
+				userId: null,
+			});
 			if (formRef.current) {
 				formRef.current.reset();
 			}
@@ -161,22 +178,24 @@ const Form: FC<FormProps> = ({
 				e.preventDefault();
 				if (formRef.current) {
 					handleSubmit({
-						userId: null,
+						userId: form.userId,
 						dateEmployed: formRef.current.dateEmployed.value,
-						user: {
-							firstName: formRef.current.firstName.value,
-							lastName: formRef.current.lastName.value,
-							email: formRef.current.email.value,
-							profile: {
-								image: formRef.current.image.files[0] || undefined,
-								address: formRef.current?.address.value,
-								dob: formRef.current?.dob.value,
-								gender: formRef.current?.gender.value,
-								phone: formRef.current?.phone.value,
-								state: formRef.current?.state.value,
-								city: formRef.current?.city.value,
-							},
-						},
+						user: !form.userId
+							? {
+									firstName: formRef.current.firstName.value,
+									lastName: formRef.current.lastName.value,
+									email: formRef.current.email.value,
+									profile: {
+										image: formRef.current.image.files[0] || undefined,
+										address: formRef.current?.address.value,
+										dob: formRef.current?.dob.value,
+										gender: formRef.current?.gender.value,
+										phone: formRef.current?.phone.value,
+										state: formRef.current?.state.value,
+										city: formRef.current?.city.value,
+									},
+							  }
+							: null,
 						department: form.department,
 						job: form.job,
 						supervisors: form.supervisors,
@@ -197,63 +216,246 @@ const Form: FC<FormProps> = ({
 					/>
 				</div>
 			)}
-			<div className="gap-2 grid grid-cols-1 md:grid-cols-2 md:gap-4 lg:gap-6">
-				<div className="w-full md:col-span-2">
+			<div className="gap-2 grid grid-cols-1 items-end md:grid-cols-2 md:gap-4 lg:gap-6">
+				<div className="flex justify-end w-full md:col-span-2">
 					<div className="w-full md:w-1/2 lg:w-1/3">
-						<File
+						<Button
 							disabled={loading}
-							error={formErrors?.image || errors?.image}
-							label="Image"
-							onChange={({ target: { files } }) => {
-								if (files && files[0]) {
-									setForm((prevState) => ({
-										...prevState,
-										image: files[0].name,
-									}));
-								}
-								removeFormErrors('image');
-							}}
-							name="image"
-							placeholder="Upload Image"
-							required={editMode ? false : true}
-							value={form.image}
+							onClick={() =>
+								setSelectUser((selected) => {
+									if (!editMode && !selected === false) {
+										setForm((prevState) => ({
+											...prevState,
+											userId: null,
+										}));
+									}
+									return !selected;
+								})
+							}
+							title={
+								selectUser
+									? editMode
+										? 'Update user details'
+										: 'Create new user'
+									: 'Select existing user'
+							}
+							type="button"
 						/>
 					</div>
 				</div>
-				<div className="w-full">
-					<Input
-						defaultValue={initState?.user.firstName}
-						disabled={loading}
-						error={formErrors?.firstName || errors?.firstName}
-						label="First Name"
-						name="firstName"
-						onChange={() => removeFormErrors('firstName')}
-						placeholder="First Name"
-					/>
-				</div>
-				<div className="w-full">
-					<Input
-						defaultValue={initState?.user.lastName}
-						disabled={loading}
-						error={formErrors?.lastName || errors?.lastName}
-						label="Last Name"
-						name="lastName"
-						onChange={() => removeFormErrors('lastName')}
-						placeholder="Last Name"
-					/>
-				</div>
-				<div className="w-full md:flex md:flex-col md:justify-end">
-					<Input
-						defaultValue={initState?.user.email}
-						disabled={loading}
-						error={formErrors?.email || errors?.email}
-						label="Email Address"
-						name="email"
-						onChange={() => removeFormErrors('email')}
-						placeholder="Email Address"
-						type="email"
-					/>
-				</div>
+				{selectUser ? (
+					<React.Fragment>
+						<div className="w-full">
+							<Select2
+								bdrColor={
+									usersError || formErrors?.userId || errors?.userId
+										? 'border-red-600'
+										: 'border-gray-300'
+								}
+								btn={{
+									caps: true,
+									disabled:
+										users.isFetching ||
+										(users.data &&
+											users.data.result.length >= users.data.total),
+									onClick: () => {
+										if (
+											users.data &&
+											users.data.total > users.data.result.length
+										) {
+											setUsrLimit(
+												(prevState) => prevState + DEFAULT_PAGINATION_SIZE
+											);
+										}
+									},
+									title: users.isFetching
+										? 'loading...'
+										: users.data && users.data.result.length >= users.data.total
+										? 'loaded all'
+										: 'load more',
+								}}
+								disabled={users.isLoading || loading}
+								onSelect={({ value }) =>
+									setForm((prevState) => ({
+										...prevState,
+										userId: value,
+									}))
+								}
+								error={usersError || formErrors?.userId || errors?.userId}
+								options={
+									users.data
+										? users.data.result
+												.reduce(
+													(
+														total: {
+															title: string;
+															value: string;
+														}[],
+														user
+													) => {
+														if (user.isActive)
+															return [
+																...total,
+																{
+																	image: user.profile?.image || DEFAULT_IMAGE,
+																	title: user.firstName + ' ' + user.lastName,
+																	value: user.id,
+																},
+															];
+														return total;
+													},
+													[]
+												)
+												.sort((a, b) => {
+													const aName = a.title.toLowerCase();
+													const bName = b.title.toLowerCase();
+													return aName < bName ? -1 : aName > bName ? 1 : 0;
+												})
+										: []
+								}
+								value={form.userId || ''}
+								required
+								label="User"
+								placeholder="Select User"
+								shadow="shadow-lg"
+							/>
+						</div>
+						<div className="hidden w-full md:block" />
+					</React.Fragment>
+				) : (
+					<React.Fragment>
+						<div className="w-full md:col-span-2">
+							<div className="w-full md:w-1/2 lg:w-1/3">
+								<File
+									disabled={loading}
+									error={formErrors?.image || errors?.image}
+									label="Image"
+									onChange={({ target: { files } }) => {
+										if (files && files[0]) {
+											setForm((prevState) => ({
+												...prevState,
+												image: files[0].name,
+											}));
+										}
+										removeFormErrors('image');
+									}}
+									name="image"
+									placeholder="Upload Image"
+									required={editMode ? false : true}
+									value={form.image}
+								/>
+							</div>
+						</div>
+						<div className="w-full">
+							<Input
+								defaultValue={initState?.user.firstName}
+								disabled={loading}
+								error={formErrors?.firstName || errors?.firstName}
+								label="First Name"
+								name="firstName"
+								onChange={() => removeFormErrors('firstName')}
+								placeholder="First Name"
+							/>
+						</div>
+						<div className="w-full">
+							<Input
+								defaultValue={initState?.user.lastName}
+								disabled={loading}
+								error={formErrors?.lastName || errors?.lastName}
+								label="Last Name"
+								name="lastName"
+								onChange={() => removeFormErrors('lastName')}
+								placeholder="Last Name"
+							/>
+						</div>
+						<div className="w-full md:flex md:flex-col md:justify-end">
+							<Input
+								defaultValue={initState?.user.email}
+								disabled={loading}
+								error={formErrors?.email || errors?.email}
+								label="Email Address"
+								name="email"
+								onChange={() => removeFormErrors('email')}
+								placeholder="Email Address"
+								type="email"
+							/>
+						</div>
+						<div className="w-full">
+							<Input
+								defaultValue={initState?.user.profile?.phone || undefined}
+								disabled={loading}
+								error={formErrors?.phone || errors?.phone}
+								label="Phone Number"
+								name="phone"
+								onChange={() => removeFormErrors('phone')}
+								placeholder="Phone Number"
+							/>
+						</div>
+						<div className="w-full">
+							<Select
+								defaultValue={initState?.user.profile?.gender || 'MALE'}
+								disabled={loading}
+								error={formErrors?.gender || errors?.gender}
+								label="Gender"
+								name="gender"
+								onChange={() => removeFormErrors('gender')}
+								options={[
+									{ title: 'Male', value: 'MALE' },
+									{ title: 'Female', value: 'FEMALE' },
+								]}
+							/>
+						</div>
+						<div className="w-full">
+							<Input
+								defaultValue={
+									initState?.user.profile?.dob
+										? getStringedDate(initState.user.profile.dob)
+										: undefined
+								}
+								disabled={loading}
+								error={formErrors?.dob || errors?.dob}
+								label="Date Of Birth"
+								name="dob"
+								onChange={() => removeFormErrors('dob')}
+								placeholder="Date Of Birth"
+								type="date"
+							/>
+						</div>
+						<div className="w-full md:col-span-2">
+							<Textarea
+								defaultValue={initState?.user.profile?.address || undefined}
+								disabled={loading}
+								error={formErrors?.address || errors?.address || ''}
+								label="Address"
+								name="address"
+								onChange={() => removeFormErrors('address')}
+								placeholder="Address"
+							/>
+						</div>
+						<div className="w-full">
+							<Input
+								defaultValue={initState?.user.profile?.state || undefined}
+								disabled={loading}
+								error={formErrors?.state || errors?.state}
+								label="State"
+								name="state"
+								onChange={() => removeFormErrors('state')}
+								placeholder="State"
+							/>
+						</div>
+						<div className="w-full">
+							<Input
+								defaultValue={initState?.user.profile?.city || undefined}
+								disabled={loading}
+								error={formErrors?.city || errors?.city}
+								label="City"
+								name="city"
+								onChange={() => removeFormErrors('city')}
+								placeholder="City"
+							/>
+						</div>
+					</React.Fragment>
+				)}
 				<div className="w-full">
 					<Select
 						btn={{
@@ -431,80 +633,6 @@ const Form: FC<FormProps> = ({
 						label="Supervisors"
 						placeholder="Select Supervisors"
 						shadow="shadow-lg"
-					/>
-				</div>
-				<div className="w-full">
-					<Input
-						defaultValue={initState?.user.profile?.phone || undefined}
-						disabled={loading}
-						error={formErrors?.phone || errors?.phone}
-						label="Phone Number"
-						name="phone"
-						onChange={() => removeFormErrors('phone')}
-						placeholder="Phone Number"
-					/>
-				</div>
-				<div className="w-full">
-					<Select
-						defaultValue={initState?.user.profile?.gender || 'MALE'}
-						disabled={loading}
-						error={formErrors?.gender || errors?.gender}
-						label="Gender"
-						name="gender"
-						onChange={() => removeFormErrors('gender')}
-						options={[
-							{ title: 'Male', value: 'MALE' },
-							{ title: 'Female', value: 'FEMALE' },
-						]}
-					/>
-				</div>
-				<div className="w-full md:col-span-2">
-					<Textarea
-						defaultValue={initState?.user.profile?.address || undefined}
-						disabled={loading}
-						error={formErrors?.address || errors?.address || ''}
-						label="Address"
-						name="address"
-						onChange={() => removeFormErrors('address')}
-						placeholder="Address"
-					/>
-				</div>
-				<div className="w-full">
-					<Input
-						defaultValue={initState?.user.profile?.state || undefined}
-						disabled={loading}
-						error={formErrors?.state || errors?.state}
-						label="State"
-						name="state"
-						onChange={() => removeFormErrors('state')}
-						placeholder="State"
-					/>
-				</div>
-				<div className="w-full">
-					<Input
-						defaultValue={initState?.user.profile?.city || undefined}
-						disabled={loading}
-						error={formErrors?.city || errors?.city}
-						label="City"
-						name="city"
-						onChange={() => removeFormErrors('city')}
-						placeholder="City"
-					/>
-				</div>
-				<div className="w-full">
-					<Input
-						defaultValue={
-							initState?.user.profile?.dob
-								? getStringedDate(initState.user.profile.dob)
-								: undefined
-						}
-						disabled={loading}
-						error={formErrors?.dob || errors?.dob}
-						label="Date Of Birth"
-						name="dob"
-						onChange={() => removeFormErrors('dob')}
-						placeholder="Date Of Birth"
-						type="date"
 					/>
 				</div>
 				<div className="w-full">
