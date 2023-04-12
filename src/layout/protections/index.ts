53aee5a46cff09a5e38ef63bcadd3b477d404198
kingkite <kitewebdevelopers@gmail.com>
2023-04-12 21:31:16 +0100

    fixed import error

--- src/layout/protections/index.ts
+++ src/layout/protections/index.ts
@@ -1,3 +1,3 @@
 export { default as Authenticated } from './authenticated';
 export { default as CheckAuth } from './check-auth';
-export { default as NotAuthenticated } from './not-authenticatedd';
+export { default as NotAuthenticated } from './not-authenticated';
