1558657fe8220154dfa06ac02aa7a15b32670253
kingkite <kitewebdevelopers@gmail.com>
2023-04-18 22:46:51 +0100

    Fixed the error class missing messages

--- src/utils/classes/index.ts
+++ src/utils/classes/index.ts
@@ -5,19 +5,20 @@ const message404 = 'A requested resource was not foudn!';
 const message500 = 'A server error occurred! Please try again later.';
 
 export class NextApiErrorMessage extends Error {
-	message: string = '';
+	message: string;
 	status: number = 500;
 
-	constructor(status: number = 500, msg?: string) {
-		const message =
-			msg || status === 400
-				? message400
-				: status === 401
-				? message401
-				: status === 404
-				? message404
-				: message500;
+	constructor(status: number = 500, message?: string) {
 		super(message);
+		this.message = message
+			? message
+			: status === 400
+			? message400
+			: status === 401
+			? message401
+			: status === 404
+			? message404
+			: message500;
 		this.status = status;
 	}
 }
