a5eb6267ee8a3e04c4e883554692a2ea6e43a8b1
kingkite <kitewebdevelopers@gmail.com>
2023-04-21 17:19:23 +0100

    Got the convert csv to json function working

--- src/utils/files/convert.ts
+++ src/utils/files/convert.ts
@@ -1,5 +1,46 @@
 import fs from 'fs';
 
+function getRowData(raw: string) {
+	const splitRow = raw.split('');
+	const data: string[] = [];
+	let newValue = '';
+
+	splitRow.forEach((row, index) => {
+		const quotes = newValue.startsWith('"');
+		// empty value
+		if (newValue === ',') {
+			data.push('');
+			newValue = '';
+		}
+		// does not start with a '"', most probably not a string
+		if (!quotes) {
+			if (row !== ',') newValue += row;
+			else {
+				data.push(newValue);
+				newValue = '';
+			}
+		} else {
+			// starts with a '"', most probably a string
+			// if current item is a ',' and the previous item is a '"', thats a value
+			if (row === ',' && splitRow[index - 1] === '"') {
+				data.push(newValue);
+				newValue = '';
+			} else newValue += row;
+		}
+		// the last item
+		if (index === splitRow.length - 1) {
+			data.push(newValue);
+			newValue = '';
+		}
+	});
+	return data.map((item) => {
+		// Remove the first and the last '" if available
+		const quotes = item.startsWith('"') && item.endsWith('"');
+		if (!quotes) return item.trim();
+		return item.slice(1, item.length - 1).trim();
+	});
+}
+
 export function csvToJson<DataType = any>(
 	file: string,
 	options: {
@@ -51,18 +92,21 @@ export function csvToJson<DataType = any>(
 							.trim()
 							.split(',')
 							.map((item) => {
-								// Remove the first and the last '"'
-								const splitItem = item.split('"');
-								const result = splitItem
-									// .filter((h, i) => h !== '"' && i !== 0 && i !== splitItem.length - 1)
-									.filter((h, i) =>
-										h === '"' && (i === 0 || i === splitItem.length - 1)
-											? false
-											: true
-									)
-									.join('')
-									.trim();
-								return result;
+								// Remove the first and the last '" if available
+								const quotes = item.startsWith('"') && item.endsWith('"');
+								if (!quotes) return item.trim();
+								return item.slice(1, item.length - 1).trim();
+								// const splitItem = item.split('"');
+								// const result = splitItem
+								// 	// .filter((h, i) => h !== '"' && i !== 0 && i !== splitItem.length - 1)
+								// 	.filter((h, i) =>
+								// 		h === '"' && (i === 0 || i === splitItem.length - 1)
+								// 			? false
+								// 			: true
+								// 	)
+								// 	.join('')
+								// 	.trim();
+								// return result;
 							});
 
 						const columnLength = options.headers
@@ -89,50 +133,31 @@ export function csvToJson<DataType = any>(
 							});
 
 							if (!requiredHeaders) {
+								const data =
+									invalidHeaders.length > 1
+										? `Some columns are invalid. '${invalidHeaders.join(
+												','
+										  )}' are not valid columns.`
+										: `A column is invalid. ${invalidHeaders.join(
+												','
+										  )} is not valid column.`;
 								reject({
 									status: 400,
-									data: `Some columns are invalid. ${invalidHeaders.join(
-										','
-									)} are not valid columns`,
+									data,
 								});
 							}
 						}
 
-						/**
-						 * Next the content.
-						 * The items excluding the 0th index/first item contains the fields for each row
-						 * Note that each row will be a string to trim the white spaces at the start and end
-						 * and split the array using the ',' delimiter/separator
-						 * and remove the '"' at the start and end. Check the header example above for a reference.
-						 * Finally join each field into a single array to represent that specifir row
-						 *
-						 */
-
+						// Next Get the rows and the fields in them
 						const rows = info.reduce((acc: string[][], row, index) => {
-							if (index === 0) return acc; // ignore the header row
-							const rowContent = row
-								.trim()
-								.split(',')
-								.map((field) => {
-									// Remove the first and the last '"'
-									const splitField = field.split('"');
-									const fieldContent = splitField
-										// .filter((h, i) => h !== '"' && i !== 0 && i !== splitField.length - 1)
-										.filter((h, i) =>
-											h === '"' && (i === 0 || i === splitField.length - 1)
-												? false
-												: true
-										)
-										.join('');
-									return fieldContent;
-								});
-							// Join the row into a single array
-							return [...acc, rowContent];
+							if (index === 0) return acc; //ignore the header row
+							if (row === '' && index === info.length - 1) return acc; // empty last line
+							const data = getRowData(row);
+							return [...acc, data];
 						}, []);
 
 						const result: DataType[] = rows.map((fields, index) => {
 							if (headers.length !== fields.length) {
-								console.log(fields);
 								reject({
 									status: 400,
 									data: `Error at row ${
