6e75f77d41d79cbfc2b7f04b16612b01d57b0670
kingkite <kitewebdevelopers@gmail.com>
2023-04-30 05:52:02 +0100

    added api route to update and get single notification

--- src/pages/api/notifications/[id].ts
+++ src/pages/api/notifications/[id].ts
@@ -1,38 +1,109 @@
 import { prisma } from '../../../db';
 import { auth } from '../../../middlewares';
+import { NextApiErrorMessage } from '../../../utils/classes';
 
-export default auth().delete(async (req, res) => {
-	const id = req.query.id as string;
-	const notification = await prisma.notification.findUnique({
-		where: {
-			id,
-		},
-		select: {
-			recipientId: true,
-		},
-	});
-	if (!notification) {
-		return res.status(404).json({
-			status: 'error',
-			message: 'Notification with specified ID does not exist',
+export default auth()
+	.use(async (req, res, next) => {
+		const id = req.query.id as string;
+		const notification = await prisma.notification.findUnique({
+			where: {
+				id,
+			},
+			select: {
+				recipientId: true,
+			},
 		});
-	}
 
-	if (notification.recipientId !== req.user.id) {
-		return res.status(403).json({
-			status: 'error',
-			message: 'You are forbidden from making this request.',
+		if (!notification)
+			throw new NextApiErrorMessage(
+				404,
+				'Notification with specified ID does not exist'
+			);
+
+		if (notification.recipientId !== req.user.id)
+			throw new NextApiErrorMessage(403);
+		next();
+	})
+	.get(async (req, res) => {
+		const notification = await prisma.notification.findUnique({
+			where: {
+				id: req.query.id as string,
+			},
+			select: {
+				createdAt: true,
+				id: true,
+				title: true,
+				message: true,
+				messageId: true,
+				read: true,
+				recipient: {
+					select: {
+						firstName: true,
+						lastName: true,
+						email: true,
+						profile: {
+							select: {
+								image: true,
+							},
+						},
+					},
+				},
+				sender: {
+					select: {
+						firstName: true,
+						lastName: true,
+						email: true,
+						profile: {
+							select: {
+								image: true,
+							},
+						},
+					},
+				},
+				type: true,
+			},
 		});
-	}
+		if (!notification)
+			throw new NextApiErrorMessage(
+				404,
+				'Notification with the specified was not found!'
+			);
 
-	await prisma.notification.delete({
-		where: {
-			id,
-		},
-	});
+		return res.status(200).json({
+			status: 'success',
+			message: 'Fetched notification successfully.',
+			data: notification,
+		});
+	})
+	.put(async (req, res) => {
+		const { read } = req.body;
 
-	return res.status(200).json({
-		status: 'success',
-		message: 'Notification was deleted successfully!',
+		await prisma.notification.update({
+			where: {
+				id: req.query.id as string,
+			},
+			data: {
+				read: !!read,
+			},
+			select: { id: true },
+		});
+
+		return res.status(200).json({
+			status: 'success',
+			message: 'Notification updated successfully',
+		});
+	})
+	.delete(async (req, res) => {
+		const id = req.query.id as string;
+
+		await prisma.notification.delete({
+			where: {
+				id,
+			},
+		});
+
+		return res.status(200).json({
+			status: 'success',
+			message: 'Notification was deleted successfully!',
+		});
 	});
-});
