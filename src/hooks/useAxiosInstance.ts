b0fbac6e364998917a6497b6e5086fda86a6fce6
kingkite <kitewebdevelopers@gmail.com>
2023-04-30 05:19:19 +0100

    Updated the instance

--- src/hooks/useAxiosInstance.ts
+++ src/hooks/useAxiosInstance.ts
@@ -3,20 +3,27 @@ import React from 'react';
 import { axiosInstance } from '../utils/axios';
 import { handleAxiosErrors } from '../validators';
 
-function useAxiosInstance<DataType = any, ErrorType = any>(
-	url: string,
-	{
-		onSuccess,
-		onError,
-	}: {
-		onSuccess?: (data: DataType) => void;
-		onError?: (error: {
-			status: number;
-			message: string;
-			data?: ErrorType;
-		}) => void;
-	}
-) {
+function useAxiosInstance<DataType = any, ErrorType = any>({
+	onSettled,
+	onSuccess,
+	onError,
+}: {
+	onSuccess?: (data: {
+		status: string;
+		message: string;
+		data?: DataType;
+	}) => void;
+	onError?: (error: {
+		status: number;
+		message: string;
+		data?: ErrorType;
+	}) => void;
+	onSettled?: (response: {
+		status: 'success' | 'error';
+		message: string;
+		data?: DataType | ErrorType;
+	}) => void;
+}) {
 	const [data, setData] = React.useState<{
 		status: string;
 		message: string;
@@ -29,34 +36,48 @@ function useAxiosInstance<DataType = any, ErrorType = any>(
 	}>();
 	const [loading, setLoading] = React.useState(false);
 
-	const fetch = React.useCallback(async () => {
-		try {
-			const response = await axiosInstance({
-				url,
-				method: 'get',
-				headers: {
-					Accept: 'application/json',
-					'Content-Type': 'application/json',
-				},
-			});
-			setData(response.data);
-			if (onSuccess) onSuccess(response.data.data);
-		} catch (err) {
-			const errors = handleAxiosErrors(err) || {
-				status: 500,
-				message: (err as any).message,
-			};
-			setError(errors);
-			if (onError) onError(errors);
-		} finally {
-			setLoading(false);
-		}
-	}, [url, onSuccess, onError]);
+	const execute = React.useCallback(
+		async (url: string) => {
+			try {
+				const response = await axiosInstance({
+					url,
+					method: 'get',
+					headers: {
+						Accept: 'application/json',
+						'Content-Type': 'application/json',
+					},
+				});
+				setData(response.data);
+				if (onSuccess) onSuccess(response.data);
+				if (onSettled)
+					onSettled({
+						...response.data,
+						status: 'success',
+					});
+			} catch (err) {
+				const errors = handleAxiosErrors(err) || {
+					status: 500,
+					message: (err as any).message,
+				};
+				setError(errors);
+				if (onError) onError(errors);
+				if (onSettled)
+					onSettled({
+						...errors,
+						status: 'error',
+					});
+			} finally {
+				setLoading(false);
+			}
+		},
+		[onSettled, onSuccess, onError]
+	);
 
 	return {
 		data,
 		error,
 		loading,
+		execute,
 	};
 }
 
