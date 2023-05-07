36ac46771507dd6c19771961f36b5804bf241a99
kingkite <kitewebdevelopers@gmail.com>
2023-05-07 21:26:44 +0100

    Return a value in the resolve and made callbacks optional

--- src/utils/files/convert-zip-csv-json.ts
+++ src/utils/files/convert-zip-csv-json.ts
@@ -5,18 +5,23 @@ import csvToJson from './convert-csv';
 import { exportPermissionHeaders as permissionHeaders } from '../../config';
 import { ObjectPermissionImportType } from '../../types';
 
+type ConvertReturnType<DataType> = {
+	data: DataType[];
+	permissions: ObjectPermissionImportType[];
+};
+
 export default function convertZip<DataType = any>(
 	file: string,
 	options: {
 		headers?: string[];
 		name: string;
-		onLoadData: (data: DataType[]) => Promise<any>;
-		onLoadPermissions: (
+		onLoadData?: (data: DataType[]) => Promise<any>;
+		onLoadPermissions?: (
 			permissions: ObjectPermissionImportType[]
 		) => Promise<any>;
 	}
 ) {
-	return new Promise((resolve, reject) => {
+	return new Promise<ConvertReturnType<DataType>>((resolve, reject) => {
 		try {
 			fs.readFile(file, async function (err, info) {
 				if (err) throw err;
@@ -37,21 +42,30 @@ export default function convertZip<DataType = any>(
 						message: 'zip file does not contain the permissions.csv file.',
 					});
 				}
+				const result: ConvertReturnType<DataType> = {
+					data: [],
+					permissions: [],
+				};
 				csvToJson(data, {
 					headers: options.headers,
 					isPath: false,
 				})
-					.then((data: DataType[]) => options.onLoadData(data))
+					.then((data: DataType[]) => {
+						result.data = data;
+						if (options.onLoadData) return options.onLoadData(data);
+					})
 					.then(() =>
 						csvToJson(permissions, {
 							headers: permissionHeaders,
 							isPath: false,
 						})
 					)
-					.then((data: ObjectPermissionImportType[]) =>
-						options.onLoadPermissions(data)
-					)
-					.then(() => resolve(undefined))
+					.then((data: ObjectPermissionImportType[]) => {
+						result.permissions = data;
+						if (options.onLoadPermissions)
+							return options.onLoadPermissions(data);
+					})
+					.then(() => resolve(result))
 					.catch((error) => reject(error));
 			});
 		} catch (error) {
