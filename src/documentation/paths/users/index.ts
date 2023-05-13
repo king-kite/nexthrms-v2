a367cab28cfdc13ba76ec797982562dbe05cf633
kingkite <kitewebdevelopers@gmail.com>
2023-05-13 21:05:34 +0100

    Added documentation for import users api url

--- src/documentation/paths/users/index.ts
+++ src/documentation/paths/users/index.ts
@@ -1,12 +1,13 @@
 import * as routes from '../../../config/server';
 
-import exportUsers from './exportUsers';
-import userActivation from './userActivation';
-import passwordChange from './passwordChange';
+import exportUsers from './export-users';
+import importUsers from './import-users';
+import userActivation from './user-activation';
+import passwordChange from './password-change';
 import user from './user'; // Single User Path
 import users from './users';
-import userGroups from './userGroups';
-import userPermissions from './userPermissions';
+import userGroups from './user-groups';
+import userPermissions from './user-permissions';
 
 const paths = {
 	[routes.USERS_URL]: users,
@@ -16,6 +17,7 @@ const paths = {
 	[routes.CHANGE_USER_PASSWORD_URL]: passwordChange,
 	[routes.ACTIVATE_USER_URL]: userActivation,
 	[routes.USERS_EXPORT_URL]: exportUsers,
+	[routes.USERS_IMPORT_URL]: importUsers,
 };
 
 export default paths;
