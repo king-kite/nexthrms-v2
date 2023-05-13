dc096e3b391503a07b2cb7483593a65fc966275a
kingkite <kitewebdevelopers@gmail.com>
2023-05-13 12:32:34 +0100

    Created some table components

---
+++ src/components/common/table/components.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+
+function classNames(...classes: string[]) {
+	return classes.filter(Boolean).join(' ');
+}
+
+export function Button({
+	children,
+	className = '',
+	...rest
+}: {
+	children: React.ReactNode;
+	className?: string;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>) {
+	return (
+		<button
+			className={classNames(
+				'bg-white border border-gray-300 font-medium inline-flex items-center px-4 py-2 relative rounded-md text-gray-700 text-sm hover:bg-gray-50',
+				className
+			)}
+			type="button"
+			{...rest}
+		>
+			{children}
+		</button>
+	);
+}
+
+export function PageButton({
+	active,
+	children,
+	className = '',
+	disabled,
+	...rest
+}: {
+	children: React.ReactNode;
+	className?: string;
+	disabled?: boolean;
+	active?: boolean;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>) {
+	return (
+		<button
+			type="button"
+			className={classNames(
+				`border border-gray-300 ${
+					disabled
+						? 'bg-gray-200 cursor-not-allowed text-gray-500'
+						: active
+						? 'bg-primary-500 text-gray-100'
+						: 'bg-white text-gray-500 hover:bg-gray-100'
+				} font-medium inline-flex items-center p-2 relative text-sm`,
+				className
+			)}
+			disabled={disabled}
+			{...rest}
+		>
+			{children}
+		</button>
+	);
+}
+
+// export function SortIcon({ className }) {
+// 	return (
+// 		<svg
+// 			className={className}
+// 			stroke="currentColor"
+// 			fill="currentColor"
+// 			strokeWidth="0"
+// 			viewBox="0 0 320 512"
+// 			height="1em"
+// 			width="1em"
+// 			xmlns="http://www.w3.org/2000/svg"
+// 		>
+// 			<path d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41zm255-105L177 64c-9.4-9.4-24.6-9.4-33.9 0L24 183c-15.1 15.1-4.4 41 17 41h238c21.4 0 32.1-25.9 17-41z"></path>
+// 		</svg>
+// 	);
+// }
+
+// export function SortUpIcon({ className }) {
+// 	return (
+// 		<svg
+// 			className={className}
+// 			stroke="currentColor"
+// 			fill="currentColor"
+// 			strokeWidth="0"
+// 			viewBox="0 0 320 512"
+// 			height="1em"
+// 			width="1em"
+// 			xmlns="http://www.w3.org/2000/svg"
+// 		>
+// 			<path d="M279 224H41c-21.4 0-32.1-25.9-17-41L143 64c9.4-9.4 24.6-9.4 33.9 0l119 119c15.2 15.1 4.5 41-16.9 41z"></path>
+// 		</svg>
+// 	);
+// }
+
+// export function SortDownIcon({ className }) {
+// 	return (
+// 		<svg
+// 			className={className}
+// 			stroke="currentColor"
+// 			fill="currentColor"
+// 			strokeWidth="0"
+// 			viewBox="0 0 320 512"
+// 			height="1em"
+// 			width="1em"
+// 			xmlns="http://www.w3.org/2000/svg"
+// 		>
+// 			<path d="M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41z"></path>
+// 		</svg>
+// 	);
+// }
---
+++ src/components/common/table/pagination.tsx
@@ -0,0 +1,147 @@
+import { PaginationType, Select, usePagination } from 'kite-react-tailwind';
+import React from 'react';
+import {
+	FaAngleDoubleLeft,
+	FaAngleDoubleRight,
+	FaChevronLeft,
+	FaChevronRight,
+} from 'react-icons/fa';
+
+import { Button, PageButton } from './components';
+// import { usePagination } from "../../../hooks";
+
+export type PaginationProps = PaginationType & {
+	disabled?: boolean;
+	onChange?: (pageNumber: number) => void;
+	onSizeChange?: (size: number) => void;
+	pageSizes?: number[];
+};
+
+function Pagination({
+	disabled = false,
+	onChange,
+	pageSizes = [25, 50, 100, 200],
+	onSizeChange,
+	...props
+}: PaginationProps) {
+	const { changePage, currentPage, pages, pageSize, totalPages } =
+		usePagination({
+			...props,
+			pageSize: props.pageSize || 5,
+		});
+
+	const canPreviousPage = React.useMemo(() => currentPage > 1, [currentPage]);
+	const canNextPage = React.useMemo(
+		() => currentPage < totalPages,
+		[currentPage, totalPages]
+	);
+
+	const goToPage = React.useCallback(
+		(pageNumber = 1) => {
+			if (!disabled && currentPage !== pageNumber) {
+				changePage(pageNumber);
+				if (onChange) onChange(pageNumber);
+			}
+		},
+		[currentPage, changePage, disabled, onChange]
+	);
+
+	const previousPage = React.useCallback(() => {
+		const value = currentPage - 1 <= 0 ? 1 : currentPage - 1;
+		goToPage(value);
+	}, [currentPage, goToPage]);
+
+	const nextPage = React.useCallback(() => {
+		const value = currentPage + 1 >= totalPages ? totalPages : currentPage + 1;
+		goToPage(value);
+	}, [currentPage, goToPage, totalPages]);
+
+	return (
+		<div className="flex items-center justify-between py-3">
+			<div className="flex flex-1 justify-between sm:hidden">
+				<Button onClick={previousPage} disabled={!canPreviousPage || disabled}>
+					Previous
+				</Button>
+				<Button onClick={nextPage} disabled={!canNextPage || disabled}>
+					Next
+				</Button>
+			</div>
+			<div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+				<div className="flex gap-x-2 items-center">
+					<p className="text-sm text-gray-700 tracking-wider w-full md:text-base">
+						Page <span className="font-medium">{currentPage}</span> of{' '}
+						<span className="font-medium">{totalPages}</span>
+					</p>
+					<div className="min-w-[200px] mx-2">
+						<Select
+							disabled={disabled}
+							onChange={({ target: { value } }) => {
+								if (onSizeChange) onSizeChange(+value);
+							}}
+							options={pageSizes.map((pageSize) => ({
+								title: `Show ${pageSize}`,
+								value: pageSize.toString(),
+							}))}
+							value={pageSize.toString()}
+						/>
+					</div>
+				</div>
+				<div>
+					<nav
+						className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
+						aria-label="Pagination"
+					>
+						<PageButton
+							className="rounded-l-md"
+							onClick={() => goToPage(1)}
+							disabled={!canPreviousPage || disabled}
+						>
+							<span className="sr-only">First</span>
+							<FaAngleDoubleLeft
+								className="text-xs md:text-sm"
+								aria-hidden="true"
+							/>
+						</PageButton>
+						<PageButton
+							onClick={previousPage}
+							disabled={!canPreviousPage || disabled}
+						>
+							<span className="sr-only">Previous</span>
+							<FaChevronLeft className="text-xs" aria-hidden="true" />
+						</PageButton>
+						<div className="hidden lg:flex lg:items-center">
+							{pages.map((page, index) => (
+								<PageButton
+									active={page === currentPage}
+									className="px-3"
+									disabled={disabled}
+									key={index}
+									onClick={() => goToPage(page)}
+								>
+									{page}
+								</PageButton>
+							))}
+						</div>
+						<PageButton onClick={nextPage} disabled={!canNextPage || disabled}>
+							<span className="sr-only">Next</span>
+							<FaChevronRight className="text-xs" aria-hidden="true" />
+						</PageButton>
+						<PageButton
+							className="rounded-r-md"
+							onClick={() => goToPage(totalPages)}
+							disabled={!canNextPage || disabled}
+						>
+							<span className="sr-only">Last</span>
+							<FaAngleDoubleRight
+								className="text-xs md:text-sm"
+								aria-hidden="true"
+							/>
+						</PageButton>
+					</nav>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Pagination;
