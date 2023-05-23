f691458de97ee20f41885f7883b7cee888302b02
kingkite <kitewebdevelopers@gmail.com>
2023-05-23 20:51:59 +0100

    Refactored the file requirements

--- src/components/Projects/Details/ImportExport.tsx
+++ src/components/Projects/Details/ImportExport.tsx
@@ -2,6 +2,7 @@ import { Alert, Button, ButtonDropdown } from 'kite-react-tailwind';
 import React from 'react';
 import { FaCloudDownloadAlt, FaCloudUploadAlt } from 'react-icons/fa';
 
+import { fileRequirements as requirements } from '../requirements';
 import { ImportForm, Modal } from '../../common';
 import {
 	permissions,
@@ -112,59 +113,7 @@ function ImportExport({ id }: { id: string }) {
 									});
 									setModalVisible(false);
 								}}
-								requirements={[
-									{
-										required: false,
-										title: 'id',
-										value: 'c2524fca-9182-4455-8367-c7a27abe1b73',
-									},
-									{
-										title: 'name',
-										value: 'Image 1',
-									},
-									{
-										title: 'file',
-										value: '/images/default.png',
-									},
-									{
-										title: 'size',
-										value: '1607',
-									},
-									{
-										required: false,
-										title: 'storage_info_keys',
-										value: '"id,name,type"',
-									},
-									{
-										required: false,
-										title: 'storage_info_values',
-										value:
-											'"c2524fca-9182-4455-8367-c7a27abe1b73,Image 1,file"',
-									},
-									{
-										title: 'type',
-										value: 'image',
-									},
-									{
-										title: 'project_id',
-										value: 'c2524fca-9182-4455-8367-c7a27abe1b73',
-									},
-									{
-										required: false,
-										title: 'uploaded_by',
-										value: 'c2524fca-9182-4455-8367-c7a27abe1b73',
-									},
-									{
-										required: false,
-										title: 'updated_at',
-										value: '"2023-03-26T21:49:51.090Z"',
-									},
-									{
-										required: false,
-										title: 'created_at',
-										value: '"2023-03-26T21:49:51.090Z"',
-									},
-								]}
+								requirements={requirements}
 								sample={samples.projectFiles}
 								url={importUrl}
 							/>
--- src/components/Projects/requirements.ts
+++ src/components/Projects/requirements.ts
@@ -1,3 +1,57 @@
+export const fileRequirements = [
+  {
+    required: false,
+    title: 'id',
+    value: 'c2524fca-9182-4455-8367-c7a27abe1b73',
+  },
+  {
+    title: 'name',
+    value: 'Image 1',
+  },
+  {
+    title: 'file',
+    value: '/images/default.png',
+  },
+  {
+    title: 'size',
+    value: '1607',
+  },
+  {
+    required: false,
+    title: 'storage_info_keys',
+    value: '"id,name,type"',
+  },
+  {
+    required: false,
+    title: 'storage_info_values',
+    value:
+      '"c2524fca-9182-4455-8367-c7a27abe1b73,Image 1,file"',
+  },
+  {
+    title: 'type',
+    value: 'image',
+  },
+  {
+    title: 'project_id',
+    value: 'c2524fca-9182-4455-8367-c7a27abe1b73',
+  },
+  {
+    required: false,
+    title: 'uploaded_by',
+    value: 'c2524fca-9182-4455-8367-c7a27abe1b73',
+  },
+  {
+    required: false,
+    title: 'updated_at',
+    value: '2023-03-26T21:49:51.090Z',
+  },
+  {
+    required: false,
+    title: 'created_at',
+    value: '2023-03-26T21:49:51.090Z',
+  },
+]
+
 export const followerRequirements = [
   {
     required: false,
@@ -57,6 +111,36 @@ export const taskRequirements = [
   },
   {
     title: 'project_id',
+    value: 'c2524fca-9182-4455-8367-c7a27abe1b73',
+  },
+  {
+    required: false,
+    title: 'updated_at',
+    value: '2023-03-26T21:49:51.090Z',
+  },
+  {
+    required: false,
+    title: 'created_at',
+    value: '2023-03-26T21:49:51.090Z',
+  },
+]
+
+export const teamRequirements = [
+  {
+    required: false,
+    title: 'id',
+    value: 'c2524fca-9182-4455-8367-c7a27abe1b73',
+  },
+  {
+    title: 'is_leader',
+    value: 'true',
+  },
+  {
+    title: 'project_id',
+    value: 'c2524fca-9182-4455-8367-c7a27abe1b73',
+  },
+  {
+    title: 'employee_id',
     value: 'c2524fca-9182-4455-8367-c7a27abe1b73',
   },
   {
