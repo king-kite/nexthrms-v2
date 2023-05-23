7031051195ebfdf56abd1ccde1c804354e4a0dc1
kingkite <kitewebdevelopers@gmail.com>
2023-05-23 21:05:29 +0100

    Fixed the topbar for projects

--- src/components/Projects/Topbar.tsx
+++ src/components/Projects/Topbar.tsx
@@ -1,8 +1,19 @@
 import { Button, ButtonDropdown, InputButton } from 'kite-react-tailwind';
 import React from 'react';
-import { FaCloudDownloadAlt, FaCloudUploadAlt, FaSearch, FaPlus } from 'react-icons/fa';
+import {
+	FaCloudDownloadAlt,
+	FaCloudUploadAlt,
+	FaSearch,
+	FaPlus,
+} from 'react-icons/fa';
 
-import { fileRequirements, followerRequirements, projectRequirements, taskRequirements, teamRequirements } from './requirements';
+import {
+	fileRequirements,
+	followerRequirements,
+	projectRequirements,
+	taskRequirements,
+	teamRequirements,
+} from './requirements';
 import { ExportForm, ImportForm, Modal } from '../common';
 import { permissions, samples, PROJECTS_IMPORT_URL } from '../../config';
 import { useAuthContext, useAlertContext } from '../../store/contexts';
@@ -21,129 +32,162 @@ type TopbarProps = {
 const Topbar = ({ loading, openModal, onSubmit, exportData }: TopbarProps) => {
 	const searchRef = React.useRef<HTMLInputElement | null>(null);
 
-	const [formType, setFormType] = React.useState<'projects' | 'files' | 'team' | 'tasks' | 'followers'>('projects');
+	const [formType, setFormType] = React.useState<
+		'projects' | 'files' | 'team' | 'tasks' | 'followers'
+	>('projects');
 	const [modalVisible, setModalVisible] = React.useState(false);
 
 	const { open } = useAlertContext();
 	const { data: authData } = useAuthContext();
 
-	const { description, requirements, sampleUrl, title, url } = React.useMemo(() => {
-		const url = PROJECTS_IMPORT_URL;
-		if (formType === 'files') {
-			return {
-				url: url + '?import=files',
-				requirements: fileRequirements,
-				title: 'Files',
-				sampleUrl: samples.projectFiles,
-				description:
-					'Upload a valid file to import multiple files for multiple projects.',
+	const { description, requirements, sampleUrl, title, url } =
+		React.useMemo(() => {
+			const url = PROJECTS_IMPORT_URL;
+			if (formType === 'files') {
+				return {
+					url: url + '?import=files',
+					requirements: fileRequirements,
+					title: 'Files',
+					sampleUrl: samples.projectFiles,
+					description:
+						'Upload a valid file to import multiple files for multiple projects.',
+				};
 			}
-		}
-		if (formType === 'team') {
-			return {
-				url: url + '?import=team',
-				requirements: teamRequirements,
-				title: 'Team',
-				sampleUrl: samples.projectTeam,
-				description:
-					'Upload a valid file to import multiple teams for multiple projects.',
+			if (formType === 'team') {
+				return {
+					url: url + '?import=team',
+					requirements: teamRequirements,
+					title: 'Team',
+					sampleUrl: samples.projectTeam,
+					description:
+						'Upload a valid file to import multiple teams for multiple projects.',
+				};
+			} else if (formType === 'tasks') {
+				return {
+					url: url + '?import=tasks',
+					requirements: taskRequirements,
+					title: 'Tasks',
+					sampleUrl: samples.projectTasks,
+					description:
+						'Upload a valid file to import multiple tasks for multiple projects.',
+				};
+			} else if (formType === 'followers') {
+				return {
+					url: url + '?import=followers',
+					requirements: followerRequirements,
+					sampleUrl: samples.projectTaskFollowers,
+					title: 'Tasks Followers',
+					description:
+						'Upload a valid file to import multiple task followers for multiple tasks.',
+				};
 			}
-		} else if (formType === 'tasks') {
 			return {
-				url: url + '?import=tasks',
-				requirements: taskRequirements,
-				title: 'Tasks',
-				sampleUrl: samples.projectTasks,
-				description:
-					'Upload a valid file to import multiple tasks for multiple projects.',
+				url: url + '?import=projects',
+				requirements: projectRequirements,
+				title: 'Projects',
+				sampleUrl: samples.projects,
+				description: 'Upload a valid file to import multiple projects.',
 			};
-		} else if (formType === 'followers') {
-			return {
-				url: url + '?import=followers',
-				requirements: followerRequirements,
-				sampleUrl: samples.projectTaskFollowers,
-				title: 'Tasks Followers',
-				description:
-					'Upload a valid file to import multiple task followers for multiple tasks.',
-			};
-		}
-		return {
-			url: url + '?import=projects',
-			requirements: projectRequirements,
-			title: 'Projects',
-			sampleUrl: samples.projects,
-			description:
-				'Upload a valid file to import multiple projects.',			
-		}
-	}, [formType]);
+		}, [formType]);
 
-	const [canCreate, canCreateTask, canCreateProjectFile, canEditProject, canEditTask] = React.useMemo(() => {
+	const [
+		canCreateProject,
+		canCreateTask,
+		canCreateProjectFile,
+		canEditProject,
+		canEditTask,
+	] = React.useMemo(() => {
 		const canCreateProject = authData
 			? authData.isSuperUser ||
 			  hasModelPermission(authData.permissions, [permissions.project.CREATE])
 			: false;
 		const canCreateTask = authData
 			? authData.isSuperUser ||
-			  authData.isAdmin && hasModelPermission(authData.permissions, [permissions.projecttask.CREATE])
+			  (authData.isAdmin &&
+					hasModelPermission(authData.permissions, [
+						permissions.projecttask.CREATE,
+					]))
 			: false;
 		const canCreateProjectFile = authData
 			? authData.isSuperUser ||
-			  authData.isAdmin && hasModelPermission(authData.permissions, [permissions.projectfile.CREATE])
+			  (authData.isAdmin &&
+					hasModelPermission(authData.permissions, [
+						permissions.projectfile.CREATE,
+					]))
 			: false;
 		const canEditProject = authData
 			? authData.isSuperUser ||
-			  authData?.isAdmin && hasModelPermission(authData.permissions, [permissions.project.EDIT])
+			  (authData?.isAdmin &&
+					hasModelPermission(authData.permissions, [permissions.project.EDIT]))
 			: false;
 		const canEditTask = authData
 			? authData.isSuperUser ||
-			  authData.isAdmin && hasModelPermission(authData.permissions, [permissions.projecttask.EDIT])
+			  (authData.isAdmin &&
+					hasModelPermission(authData.permissions, [
+						permissions.projecttask.EDIT,
+					]))
 			: false;
-		return [canCreateProject, canCreateTask, canCreateProjectFile, canEditProject, canEditTask];
+		return [
+			canCreateProject,
+			canCreateTask,
+			canCreateProjectFile,
+			canEditProject,
+			canEditTask,
+		];
 	}, [authData]);
 
 	const actionList = React.useMemo(() => {
 		const list: {
 			onClick: () => void;
 			title: string;
-		} = []
+		}[] = [];
+		if (!authData?.isAdmin && !authData?.isSuperUser) return list;
+
 		// Can create project
-		if (canCreateProject) 
+		if (canCreateProject)
 			list.push({
 				onClick() {
 					setFormType('projects');
 				},
 				title: 'Projects',
-			})
+			});
 		if (canCreateProjectFile)
 			list.push({
 				onClick() {
 					setFormType('files');
 				},
 				title: 'Files',
-			})
+			});
 		if (canCreateProject || canEditProject)
 			list.push({
 				onClick() {
 					setFormType('team');
 				},
-				title: 'Team'
-			})
-		if (canCreateTask) 
+				title: 'Team',
+			});
+		if (canCreateTask)
 			list.push({
 				onClick() {
 					setFormType('tasks');
 				},
 				title: 'Tasks',
-			})
+			});
 		if (canCreateTask || canEditTask)
 			list.push({
 				onClick() {
 					setFormType('followers');
 				},
-				title: 'Task Followers'
-			})
-		return list
-	}, [canCreateProject, canCreateTask, canCreateProjectFile, canEditProject, canEditTask])
+				title: 'Task Followers',
+			});
+		return list;
+	}, [
+		authData,
+		canCreateProject,
+		canCreateTask,
+		canCreateProjectFile,
+		canEditProject,
+		canEditTask,
+	]);
 
 	return (
 		<>
@@ -209,12 +253,12 @@ const Topbar = ({ loading, openModal, onSubmit, exportData }: TopbarProps) => {
 				{actionList.length > 0 && (
 					<div className="my-3 w-full sm:pl-1 sm:w-1/3 md:mb-0 md:mt-5 md:pl-0 md:w-1/4 lg:my-0 lg:w-1/5">
 						<ButtonDropdown
-							dropList={actionList.map(action => ({
+							dropList={actionList.map((action) => ({
 								...action,
 								onClick: () => {
 									action.onClick();
 									setModalVisible(true);
-								}
+								},
 							}))}
 							props={{
 								caps: true,
