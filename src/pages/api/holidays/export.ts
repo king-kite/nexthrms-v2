39a6ecaa53b4198e44f9993954bb55a458beb4f6
kingkite <kitewebdevelopers@gmail.com>
2023-05-24 21:17:43 +0100

    Updated the export holidays api function

--- src/pages/api/holidays/export.ts
+++ src/pages/api/holidays/export.ts
@@ -1,13 +1,61 @@
-import excelJS from 'exceljs';
-import { parse } from 'json2csv';
-
-import { permissions } from '../../../config';
+import { holidayHeaders as headers, permissions } from '../../../config';
 import { getHolidays } from '../../../db';
-import { getRecords } from '../../../db/utils';
+import {
+	createNotification,
+	exportData,
+	getObjectPermissionExportData,
+	getRecords,
+} from '../../../db/utils';
 import { admin } from '../../../middlewares';
 import { employeeMiddleware as employee } from '../../../middlewares/api';
+import {
+	GetHolidaysResponseType,
+	NextApiRequestExtendUser,
+} from '../../../types';
 import { hasModelPermission } from '../../../utils';
 import { NextApiErrorMessage } from '../../../utils/classes';
+import { handlePrismaErrors } from '../../../validators';
+
+// Get the records from the database, including the permissions
+async function getData(req: NextApiRequestExtendUser) {
+	const placeholder: GetHolidaysResponseType['data'] = {
+		total: 0,
+		result: [],
+	};
+
+	const result = await getRecords<GetHolidaysResponseType['data']>({
+		model: 'holiday',
+		perm: 'holiday',
+		query: req.query,
+		user: req.user,
+		placeholder,
+		getData(params) {
+			return getHolidays(params);
+		},
+	});
+
+	const data = result ? result.data : placeholder;
+
+	const values = data.result.map((value) => {
+		return {
+			id: value.id,
+			name: value.name,
+			date: value.date,
+			updated_at: value.updatedAt,
+			created_at: value.createdAt,
+		};
+	});
+
+	const perms = await getObjectPermissionExportData({
+		ids: values.map((value) => value.id),
+		model: 'holiday',
+	});
+
+	return {
+		data: values,
+		permissions: perms,
+	};
+}
 
 export default admin()
 	.use(employee)
@@ -17,68 +65,43 @@ export default admin()
 			hasModelPermission(req.user.allPermissions, [permissions.holiday.EXPORT]);
 		if (!hasPerm) throw new NextApiErrorMessage(403);
 
-		const placeholder = {
-			total: 0,
-			result: [],
-		};
+		getData(req)
+			.then((data) => {
+				return exportData(data, headers, {
+					type: (req.query.type as string) || 'csv',
+					userId: req.user.id,
+				});
+			})
+			.then((data) => {
+				let message =
+					'File exported successfully. Click on the download link to proceed!';
+				if (data.size) {
+					const size = String(data.size / (1024 * 1024));
+					const sizeString =
+						size.split('.')[0] + '.' + size.split('.')[1].slice(0, 2);
+					message = `File (${sizeString}MB) exported successfully. Click on the download link to proceed!`;
+				}
+				createNotification({
+					message,
+					messageId: data.file,
+					recipient: req.user.id,
+					title: 'Holiday data export was successful.',
+					type: 'DOWNLOAD',
+				});
+			})
+			.catch((err) => {
+				const error = handlePrismaErrors(err);
+				createNotification({
+					message: error.message,
+					recipient: req.user.id,
+					title: 'Holiday data export failed.',
+					type: 'ERROR',
+				});
+			});
 
-		const result = await getRecords({
-			model: 'holiday',
-			perm: 'holiday',
-			user: req.user,
-			query: req.query,
-			placeholder,
-			getData(params) {
-				return getHolidays(params);
-			},
+		return res.status(200).json({
+			status: 'success',
+			message:
+				'Your request was received successfully. A notification will be sent to you with a download link.',
 		});
-
-		const data = result ? result.data : placeholder;
-
-		const holidays = data.result.map((holiday) => ({
-			id: holiday.id,
-			name: holiday.name,
-			date: holiday.date,
-		}));
-
-		if (req.query.type === 'csv') {
-			const data = parse(holidays);
-
-			res.setHeader('Content-Type', 'text/csv');
-			res.setHeader(
-				'Content-Disposition',
-				'attachment; filename="holidays.csv"'
-			);
-
-			return res.status(200).end(data);
-		} else {
-			const workbook = new excelJS.Workbook(); // Create a new workbook
-			const worksheet = workbook.addWorksheet('Holidays'); // New Worksheet
-
-			worksheet.columns = [
-				{ header: 'ID', key: 'id', width: 10 },
-				{ header: 'Name', key: 'name', width: 10 },
-				{ header: 'Date', key: 'date', width: 10 },
-			];
-
-			worksheet.addRows(holidays);
-
-			// Making first line in excel bold
-			worksheet.getRow(1).eachCell((cell) => {
-				cell.font = { bold: true };
-			});
-
-			res.setHeader(
-				'Content-Type',
-				'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-			);
-			res.setHeader(
-				'Content-Disposition',
-				'attachment; filename="holidays.xlsx"'
-			);
-
-			return workbook.xlsx.write(res).then(function () {
-				res.status(200).end();
-			});
-		}
 	});
