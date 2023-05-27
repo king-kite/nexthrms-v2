e2836b7a3bdb01949625db4bad0a2d0a613add4d
kingkite <kitewebdevelopers@gmail.com>
2023-05-27 11:06:16 +0100

    Can export overtime data asynchronously

--- src/pages/api/overtime/admin/export.ts
+++ src/pages/api/overtime/admin/export.ts
@@ -1,13 +1,86 @@
-import excelJS from 'exceljs';
-import { parse } from 'json2csv';
-
-import { permissions } from '../../../../config';
+import { overtimeHeaders as headers, permissions } from '../../../../config';
 import { getAllOvertimeAdmin } from '../../../../db';
-import { getRecords } from '../../../../db/utils';
+import {
+	createNotification,
+	exportData,
+	getObjectPermissionExportData,
+	getRecords,
+} from '../../../../db/utils';
 import { admin } from '../../../../middlewares';
-import { GetAllOvertimeResponseType } from '../../../../types';
+import { NextApiRequestExtendUser, OvertimeType } from '../../../../types';
 import { hasModelPermission } from '../../../../utils';
 import { NextApiErrorMessage } from '../../../../utils/classes';
+import { handlePrismaErrors } from '../../../../validators';
+
+type OvertimeExportType = {
+	approved: number;
+	denied: number;
+	pending: number;
+	total: number;
+	result: (OvertimeType & {
+		attendance: {
+			id: string;
+		} | null;
+	})[];
+};
+
+async function getData(req: NextApiRequestExtendUser) {
+	const placeholder: OvertimeExportType = {
+		approved: 0,
+		denied: 0,
+		pending: 0,
+		result: [],
+		total: 0,
+	};
+
+	const result = await getRecords<OvertimeExportType>({
+		model: 'overtime',
+		perm: 'overtime',
+		query: req.query,
+		user: req.user,
+		placeholder,
+		getData(params) {
+			return getAllOvertimeAdmin({
+				...params,
+				select: {
+					attendance: {
+						select: {
+							id: true,
+						},
+					},
+				},
+			}) as Promise<OvertimeExportType>;
+		},
+	});
+
+	const data = result ? result.data : placeholder;
+
+	const values = data.result.map((overtime) => {
+		return {
+			id: overtime.id,
+			employee_id: overtime.employee.id,
+			date: overtime.date,
+			type: overtime.type,
+			status: overtime.status,
+			reason: overtime.reason,
+			attendance_id: overtime.attendance ? overtime.attendance.id : null,
+			created_by: overtime.createdBy ? overtime.createdBy.id : null,
+			approved_by: overtime.approvedBy ? overtime.approvedBy.id : null,
+			created_at: overtime.createdAt,
+			updated_at: overtime.updatedAt,
+		};
+	});
+
+	const perms = await getObjectPermissionExportData({
+		ids: values.map((value) => value.id),
+		model: 'overtime',
+	});
+
+	return {
+		data: values,
+		permissions: perms,
+	};
+}
 
 export default admin().get(async (req, res) => {
 	const hasExportPerm =
@@ -16,90 +89,43 @@ export default admin().get(async (req, res) => {
 
 	if (!hasExportPerm) throw new NextApiErrorMessage(403);
 
-	const placeholder: GetAllOvertimeResponseType['data'] = {
-		approved: 0,
-		denied: 0,
-		pending: 0,
-		result: [],
-		total: 0,
-	};
-
-	const result = await getRecords<GetAllOvertimeResponseType['data']>({
-		model: 'overtime',
-		perm: 'overtime',
-		query: req.query,
-		user: req.user,
-		placeholder,
-		getData(params) {
-			return getAllOvertimeAdmin(params);
-		},
-	});
-
-	const data = result ? result.data : placeholder;
-
-	const overtime = data.result.map((overtime) => {
-		return {
-			id: overtime.id,
-			employee_id: overtime.employee.id,
-			first_name: overtime.employee.user.firstName,
-			last_name: overtime.employee.user.lastName,
-			email: overtime.employee.user.email,
-			date: overtime.date,
-			type: overtime.type,
-			status: overtime.status,
-			reason: overtime.reason,
-			created_by: overtime.createdBy ? overtime.createdBy.id : null,
-			approved_by: overtime.approvedBy ? overtime.approvedBy.id : null,
-			created_at: overtime.createdAt,
-			updated_at: overtime.updatedAt,
-		};
-	});
-
-	if (req.query.type === 'csv') {
-		const data = parse(overtime);
-
-		res.setHeader('Content-Type', 'text/csv');
-		res.setHeader('Content-Disposition', 'attachment; filename="overtime.csv"');
-
-		return res.status(200).end(data);
-	} else {
-		const workbook = new excelJS.Workbook(); // Create a new workbook
-		const worksheet = workbook.addWorksheet('Overtime'); // New Worksheet
-
-		worksheet.columns = [
-			{ header: 'ID', key: 'id', width: 10 },
-			{ header: 'Employee ID', key: 'employee_id', width: 10 },
-			{ header: 'First Name', key: 'first_name', width: 10 },
-			{ header: 'Last Name', key: 'last_name', width: 10 },
-			{ header: 'E-mail', key: 'email', width: 10 },
-			{ header: 'Date', key: 'date', width: 10 },
-			{ header: 'Type', key: 'type', width: 10 },
-			{ header: 'Status', key: 'status', width: 10 },
-			{ header: 'Reason', key: 'reason', width: 10 },
-			{ header: 'Created By', key: 'created_by', width: 10 },
-			{ header: 'Approved By', key: 'approved_by', width: 10 },
-			{ header: 'Created At', key: 'created_at', width: 10 },
-			{ header: 'Updated At', key: 'updated_at', width: 10 },
-		];
-
-		worksheet.addRows(overtime);
-
-		// Making first line in excel bold
-		worksheet.getRow(1).eachCell((cell) => {
-			cell.font = { bold: true };
+	getData(req)
+		.then((data) => {
+			return exportData(data, headers, {
+				type: (req.query.type as string) || 'csv',
+				userId: req.user.id,
+			});
+		})
+		.then((data) => {
+			let message =
+				'File exported successfully. Click on the download link to proceed!';
+			if (data.size) {
+				const size = String(data.size / (1024 * 1024));
+				const sizeString =
+					size.split('.')[0] + '.' + size.split('.')[1].slice(0, 2);
+				message = `File (${sizeString}MB) exported successfully. Click on the download link to proceed!`;
+			}
+			createNotification({
+				message,
+				messageId: data.file,
+				recipient: req.user.id,
+				title: 'Overtime data export was successful.',
+				type: 'DOWNLOAD',
+			});
+		})
+		.catch((err) => {
+			const error = handlePrismaErrors(err);
+			createNotification({
+				message: error.message,
+				recipient: req.user.id,
+				title: 'Overtime data export failed.',
+				type: 'ERROR',
+			});
 		});
 
-		res.setHeader(
-			'Content-Type',
-			'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-		);
-		res.setHeader(
-			'Content-Disposition',
-			'attachment; filename="overtime.xlsx"'
-		);
-
-		return workbook.xlsx.write(res).then(function () {
-			res.status(200).end();
-		});
-	}
+	return res.status(200).json({
+		status: 'success',
+		message:
+			'Your request was received successfully. A notification will be sent to you with a download link.',
+	});
 });
