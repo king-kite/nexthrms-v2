c8993be067633788a6c65be68090b5f9b9690cc7
kingkite <kitewebdevelopers@gmail.com>
2023-05-28 19:57:38 +0100

    Renamed the components assets folder

--- src/components/Assets/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Details } from './Details';
-export { default as Form } from './Form';
-export { default as SearchForm } from './SearchForm';
-export { default as AssetTable } from './Table';
---
+++ src/components/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as Details } from './details';
+export { default as Form } from './form';
+export { default as SearchForm } from './search-form';
+export { default as AssetTable } from './table';
