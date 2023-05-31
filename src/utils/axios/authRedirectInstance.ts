47b2deedc6ca5ca7d5904ae115bbeb0771fbb334
kingkite <kitewebdevelopers@gmail.com>
2023-05-31 09:58:35 +0100

    Removed the redirect

--- src/utils/axios/authRedirectInstance.ts
+++ src/utils/axios/authRedirectInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosResponse } from 'axios';
 import Router from 'next/router';
 
-import { LOGIN_PAGE_URL } from '../../config'
+// import { LOGIN_PAGE_URL } from '../../config';
 import { isResponseWithRedirect } from '../../validators';
 
 const axiosInstance = axios.create();
@@ -13,9 +13,7 @@ function onFulfilled(response: AxiosResponse<any, any>) {
 function onRejected(error: unknown) {
 	const err = error as AxiosError;
 	if (err.response) {
-		if (err.response.status === 401) {
-			Router.push(LOGIN_PAGE_URL);
-		} else if (
+		if (
 			err.response.status === 307 &&
 			isResponseWithRedirect(err.response.data) &&
 			err.response.data?.redirect?.url
@@ -26,6 +24,23 @@ function onRejected(error: unknown) {
 	}
 	return Promise.reject(error);
 }
+
+// function onRejected(error: unknown) {
+// 	const err = error as AxiosError;
+// 	if (err.response) {
+// 		if (err.response.status === 401) {
+// 			Router.push(LOGIN_PAGE_URL);
+// 		} else if (
+// 			err.response.status === 307 &&
+// 			isResponseWithRedirect(err.response.data) &&
+// 			err.response.data?.redirect?.url
+// 		) {
+// 			// TODO: Validate the redirect url
+// 			Router.push(err.response.data.redirect.url);
+// 		}
+// 	}
+// 	return Promise.reject(error);
+// }
 
 axiosInstance.defaults.headers.common['Accept'] = 'application/json';
 axiosInstance.defaults.headers.common['Content-Type'] = 'application/json';
