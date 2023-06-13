475417e3d177fdbe133dc9662026cb9a6302bda3
kingkite <kitewebdevelopers@gmail.com>
2023-06-13 10:07:21 +0100

    Directory is not compulsory

--- src/components/file-manager/form.tsx
+++ src/components/file-manager/form.tsx
@@ -162,7 +162,7 @@ const Form = ({ directory, type = 'file', onSuccess }: FormProps) => {
 				{directory === undefined && (
 					<div className="w-full md:col-span-2">
 						<label class="mb-2 text-gray-600 text-xs md:text-sm block font-semibold" for="directory">
-							Directory<span class="text-red-500 mx-1">*</span>
+							Directory
 						</label>
 						<div className="flex items-center w-full">
 							<span className="bg-gray-200 font-semibold inline-block px-3 py-2 rounded-l rounded-r-none text-gray-500 text-xs w-[3.5rem] md:text-sm">
