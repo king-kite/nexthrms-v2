4c89b8614d7e8099c9a55410ff9708541457e0f0
kingkite <kitewebdevelopers@gmail.com>
2023-06-17 21:54:51 +0100

    Added import file to the frontend

--- src/components/file-manager/quick-actions.tsx
+++ src/components/file-manager/quick-actions.tsx
@@ -13,8 +13,15 @@ import {
 } from 'react-icons/fa';
 
 import { BoxGrid, BoxTitle } from './box-items';
-import { permissions, FILE_MANAGER_PAGE_URL, MEDIA_URL } from '../../config';
-import { useAuthContext } from '../../store/contexts';
+import { ImportForm, Modal } from '../common';
+import {
+	permissions,
+	samples,
+	FILE_MANAGER_PAGE_URL,
+	MANAGED_FILES_IMPORT_URL,
+	MEDIA_URL,
+} from '../../config';
+import { useAlertContext, useAuthContext } from '../../store/contexts';
 import { hasModelPermission } from '../../utils';
 
 function Actions({
@@ -24,7 +31,30 @@ function Actions({
 	openModal: (type: 'file' | 'folder') => void;
 	setDir: React.Dispatch<React.SetStateAction<string>>;
 }) {
+	const [modalVisible, setModalVisible] = React.useState(false);
+
+	const { open } = useAlertContext();
 	const { data: authData } = useAuthContext();
+
+	const { canExport, canImport } = React.useMemo(
+		() => ({
+			canExport: authData
+				? authData.isSuperUser ||
+				  (authData.isAdmin &&
+						hasModelPermission(authData.permissions, [
+							permissions.managedfile.EXPORT,
+						]))
+				: false,
+			canImport: authData
+				? authData.isSuperUser ||
+				  (authData.isAdmin &&
+						hasModelPermission(authData.permissions, [
+							permissions.managedfile.CREATE,
+						]))
+				: false,
+		}),
+		[authData]
+	);
 
 	const accesses = React.useMemo(
 		() => [
@@ -69,21 +99,6 @@ function Actions({
 		[setDir]
 	);
 	const actions = React.useMemo(() => {
-		const canExport = authData
-			? authData.isSuperUser ||
-			  (authData.isAdmin &&
-					hasModelPermission(authData.permissions, [
-						permissions.managedfile.EXPORT,
-					]))
-			: false;
-		const canImport = authData
-			? authData.isSuperUser ||
-			  (authData.isAdmin &&
-					hasModelPermission(authData.permissions, [
-						permissions.managedfile.CREATE,
-					]))
-			: false;
-
 		const data = [
 			{
 				bg: 'bg-gray-500',
@@ -115,7 +130,7 @@ function Actions({
 			});
 
 		return data;
-	}, [authData, openModal]);
+	}, [canImport, canExport, openModal]);
 	return (
 		<>
 			<div className="my-2 md:my-4">
@@ -126,6 +141,73 @@ function Actions({
 				<BoxTitle title="quick access" />
 				<BoxGrid actions={accesses} />
 			</div>
+			{canImport && (
+				<Modal
+					close={() => setModalVisible(false)}
+					component={
+						<ImportForm
+							onSuccess={(data) => {
+								open({
+									type: 'success',
+									message: data.message,
+								});
+								setModalVisible(false);
+							}}
+							requirements={[
+								{
+									required: false,
+									title: 'id',
+									value: 'c2524fca-9182-4455-8367-c7a27abe1b73',
+								},
+								{
+									title: 'name',
+									value: 'Image 1',
+								},
+								{
+									title: 'url',
+									value: '/images/default.png',
+								},
+								{
+									title: 'size',
+									value: '1607',
+								},
+								{
+									title: 'storage_info_keys',
+									value: '"public_id,location,name,type"',
+								},
+								{
+									title: 'storage_info_values',
+									value: '"c2524fca-9182-4455-8367-c7a27abe1b73,Image 1,file"',
+								},
+								{
+									title: 'type',
+									value: 'image',
+								},
+								{
+									required: false,
+									title: 'user_id',
+									value: 'c2524fca-9182-4455-8367-c7a27abe1b73',
+								},
+								{
+									required: false,
+									title: 'updated_at',
+									value: '2023-03-26T21:49:51.090Z',
+								},
+								{
+									required: false,
+									title: 'created_at',
+									value: '2023-03-26T21:49:51.090Z',
+								},
+							]}
+							sample={samples.managedFiles}
+							url={MANAGED_FILES_IMPORT_URL}
+						/>
+					}
+					description="Upload file to import multiple files"
+					title="Bulk Import Files"
+					visible={modalVisible}
+				/>
+			)}
 		</>
 	);
 }
