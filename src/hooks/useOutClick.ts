c0299d143b7238b03d31b218118750053fef428b
kingkite <kitewebdevelopers@gmail.com>
2023-06-18 09:31:37 +0100

    Added a default button type

--- src/hooks/useOutClick.ts
+++ src/hooks/useOutClick.ts
@@ -1,19 +1,23 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from 'react';
 
-const useOutClick = <Container extends HTMLElement, Button extends HTMLElement>() => {
+const useOutClick = <
+	Container extends HTMLElement,
+	Button extends HTMLElement = HTMLDivElement
+>() => {
 	const [visible, setVisible] = useState<boolean>(false);
 	const ref = useRef<Container>(null);
 	const buttonRef = useRef<Button>(null);
 
 	const handleMouseClick = useCallback(({ target }: Event): void => {
-		!buttonRef.current?.contains(target as Element) && !ref.current?.contains(target as Element) && setVisible(false);
+		!buttonRef.current?.contains(target as Element) &&
+			!ref.current?.contains(target as Element) &&
+			setVisible(false);
 	}, []);
 
 	useEffect(() => {
-		document.addEventListener("click", handleMouseClick, true);
+		document.addEventListener('click', handleMouseClick, true);
 
-		return () =>
-			document.removeEventListener("click", handleMouseClick, true);
+		return () => document.removeEventListener('click', handleMouseClick, true);
 	}, [handleMouseClick]);
 
 	return {
@@ -22,6 +26,6 @@ const useOutClick = <Container extends HTMLElement, Button extends HTMLElement>(
 		setVisible,
 		visible,
 	};
-}
+};
 
 export default useOutClick;
