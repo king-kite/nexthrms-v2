0c1ee35d538358685bc287a23ac03279c83127bd
kingkite <kitewebdevelopers@gmail.com>
2023-06-18 13:14:14 +0100

    Fixed the types

--- src/types/managed-files.ts
+++ src/types/managed-files.ts
@@ -24,6 +24,12 @@ export type ManagedFileType = {
 			} | null;
 		} | null;
 	} | null;
+	profile?: {
+		id: string;
+	} | null;
+	projectFile?: {
+		id: string;
+	} | null;
 	createdAt: Date | string;
 	updatedAt: Date | string;
 };
@@ -35,12 +41,6 @@ export type ManagedFileImportQueryType = {
 	size: number;
 	storage_info_keys?: string | null;
 	storage_info_values?: string | null;
-	profile?: {
-		id: string;
-	} | null;
-	projectFile?: {
-		id: string;
-	} | null;
 	type: string;
 	user_id?: string | null;
 	created_at?: Date | string | null;
