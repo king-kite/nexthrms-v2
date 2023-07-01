3e0cdf04b215a18315bdc8b638f476ec8a6eb136
kingkite <kitewebdevelopers@gmail.com>
2023-07-02 00:56:27 +0100

    Refactored the details actions to a dynamic component for clients detail page

---
+++ src/components/clients/detail-actions.tsx
@@ -0,0 +1,296 @@
+import { Button, ButtonType } from 'kite-react-tailwind';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+import {
+	FaUser,
+	FaUserCheck,
+	FaUserEdit,
+	FaUserShield,
+	FaUserSlash,
+	FaLock,
+	FaTrash,
+} from 'react-icons/fa';
+
+import UpdateForm from './update-form';
+import Modal from '../common/modal';
+import ChangePasswordForm from '../employees/detail/change-password-form';
+import permissions from '../../config/permissions';
+import {
+	CLIENT_OBJECT_PERMISSIONS_PAGE_URL,
+	USER_PAGE_URL,
+} from '../../config/routes';
+import { useAlertContext, useAuthContext } from '../../store/contexts';
+import { useDeleteClientMutation } from '../../store/queries/clients';
+import { useGetUserObjectPermissionsQuery } from '../../store/queries/permissions';
+import { useActivateUserMutation } from '../../store/queries/users';
+import { ClientType, UserObjPermType } from '../../types';
+import { hasModelPermission } from '../../utils/permission';
+
+function DetailActions({
+	data,
+	objPerm,
+	objUserPerm,
+	forwardedRef,
+}: {
+	data?: ClientType;
+	objPerm: UserObjPermType;
+	objUserPerm: UserObjPermType;
+	forwardedRef: {
+		ref: React.ForwardedRef<{
+			refreshPerm: () => void;
+		}>;
+	};
+}) {
+	const router = useRouter();
+	const id = React.useMemo(() => router.query.id as string, [router]);
+
+	const { open: showAlert } = useAlertContext();
+	const { data: authData } = useAuthContext();
+
+	const [modalVisible, setModalVisible] = React.useState(false);
+	const [formType, setFormType] = React.useState<'client' | 'password'>(
+		'client'
+	);
+
+	const {
+		data: objPermData,
+		isLoading: permLoading,
+		refetch: objPermRefetch,
+	} = useGetUserObjectPermissionsQuery(
+		{
+			modelName: 'clients',
+			objectId: id,
+		},
+		{
+			initialData() {
+				return objPerm;
+			},
+		}
+	);
+
+	// check if the user has edit user permission
+	const { data: objUserPermData, refetch: objUserPermRefetch } =
+		useGetUserObjectPermissionsQuery(
+			{
+				modelName: 'users',
+				objectId: data?.contact.id || '',
+			},
+			{
+				enabled: data && !!data.contact.id,
+				initialData() {
+					return objUserPerm;
+				},
+			}
+		);
+
+	React.useImperativeHandle(
+		forwardedRef.ref,
+		() => ({
+			refreshPerm: objPermRefetch,
+			refreshUserPerm: objUserPermRefetch,
+		}),
+		[objPermRefetch, objUserPermRefetch]
+	);
+
+	const [canEditUser, canViewUser] = React.useMemo(() => {
+		let canEdit = false;
+		let canView = false;
+
+		// Check model permissions
+		if (authData && (authData.isAdmin || authData.isSuperUser)) {
+			canEdit =
+				!!authData.isSuperUser ||
+				(!!authData.isAdmin &&
+					hasModelPermission(authData.permissions, [permissions.user.EDIT]));
+		}
+		if (authData && (authData.isAdmin || authData.isSuperUser)) {
+			canView =
+				!!authData.isSuperUser ||
+				(!!authData.isAdmin &&
+					hasModelPermission(authData.permissions, [permissions.user.VIEW]));
+		}
+
+		// If the user doesn't have model edit permissions, then check obj edit permission
+		if (!canEdit && objUserPermData) canEdit = objUserPermData.edit;
+		if (!canView && objUserPermData) canView = objUserPermData.view;
+
+		return [canEdit, canView];
+	}, [authData, objUserPermData]);
+
+	const { deleteClient, isLoading: delLoading } = useDeleteClientMutation({
+		onSuccess() {
+			router.back();
+		},
+		onError({ message }) {
+			showAlert({ type: 'success', message });
+		},
+	});
+
+	const { activate, isLoading: loading } = useActivateUserMutation();
+
+	const actions: ButtonType[] = React.useMemo(() => {
+		if (!data || !authData) return [];
+		const buttons: ButtonType[] = [];
+		const canEdit =
+			authData.isSuperUser ||
+			hasModelPermission(authData.permissions, [permissions.client.EDIT]) ||
+			(!permLoading && objPermData && objPermData.edit);
+		const canDelete =
+			authData.isSuperUser ||
+			hasModelPermission(authData.permissions, [permissions.client.DELETE]) ||
+			(!permLoading && objPermData && objPermData.delete);
+		const canViewObjectPermissions =
+			authData.isSuperUser ||
+			(authData.isAdmin &&
+				hasModelPermission(authData.permissions, [
+					permissions.permissionobject.VIEW,
+				]));
+
+		if (canViewUser)
+			buttons.push({
+				bg: 'bg-green-600 hover:bg-green-500',
+				iconLeft: FaUser,
+				link: USER_PAGE_URL(data.contact.id),
+				title: 'Contact Information',
+			});
+		if (canEdit)
+			buttons.push({
+				onClick: () => {
+					formType !== 'client' && setFormType('client');
+					setModalVisible(true);
+				},
+				disabled: loading || delLoading,
+				iconLeft: FaUserEdit,
+				title: 'Edit Client',
+			});
+		if (canEditUser)
+			buttons.push(
+				{
+					bg: 'bg-yellow-600 hover:bg-yellow-500',
+					iconLeft: FaLock,
+					disabled: loading || delLoading,
+					onClick: () => {
+						formType !== 'password' && setFormType('password');
+						setModalVisible(true);
+					},
+					title: 'Change Password',
+				},
+				{
+					bg: data.contact.isActive
+						? 'bg-gray-500 hover:bg-gray-600'
+						: 'bg-green-500 hover:bg-green-600',
+					disabled: loading || delLoading,
+					onClick: () =>
+						data?.contact
+							? activate(
+									[data.contact.email],
+									data.contact.isActive ? 'deactivate' : 'activate'
+							  )
+							: () => {},
+					iconLeft: data.contact.isActive ? FaUserSlash : FaUserCheck,
+					title: data.contact.isActive
+						? loading
+							? 'Deactivating...'
+							: 'Deactivate Client'
+						: loading
+						? 'Activating...'
+						: 'Activate Client',
+				}
+			);
+		if (canDelete)
+			buttons.push({
+				bg: 'bg-red-600 hover:bg-red-500',
+				iconLeft: FaTrash,
+				disabled: loading || delLoading,
+				onClick: () => deleteClient(data.id),
+				title: delLoading ? 'Deleting Client...' : 'Delete Client',
+			});
+		if (canViewObjectPermissions)
+			buttons.push({
+				bg: 'bg-gray-600 hover:bg-gray-500',
+				iconLeft: FaUserShield,
+				link: CLIENT_OBJECT_PERMISSIONS_PAGE_URL(id),
+				title: 'View Record Permissions',
+			});
+		return buttons;
+	}, [
+		activate,
+		authData,
+		data,
+		canEditUser,
+		canViewUser,
+		deleteClient,
+		delLoading,
+		formType,
+		permLoading,
+		objPermData,
+		id,
+		loading,
+	]);
+
+	return (
+		<>
+			<div className="flex flex-wrap p-4 w-full md:h-1/2 md:mt-auto md:pb-0 md:w-2/3">
+				{actions.map((action, index) => (
+					<div key={index} className="my-2 w-full sm:my-4 sm:px-4 sm:w-1/2">
+						<Button
+							{...action}
+							renderLinkAs={({ children, link, ...props }) => (
+								<Link href={link}>
+									<a {...props}>{children}</a>
+								</Link>
+							)}
+						/>
+					</div>
+				))}
+			</div>
+			{data && (
+				<Modal
+					close={() => setModalVisible(false)}
+					component={
+						formType === 'client' && data !== undefined ? (
+							<UpdateForm
+								onSuccess={() => {
+									showAlert({
+										type: 'success',
+										message: 'Client was updated successfully!',
+									});
+									setModalVisible(false);
+								}}
+								client={data}
+							/>
+						) : formType === 'password' && data ? (
+							<ChangePasswordForm
+								onSuccess={() => {
+									setModalVisible(false);
+									showAlert({
+										type: 'success',
+										message: 'Password changed successfully!',
+									});
+								}}
+								email={data.contact.email}
+							/>
+						) : (
+							<></>
+						)
+					}
+					description={
+						formType === 'password'
+							? 'Fill the form to change client password'
+							: 'Fill in the form to update client information'
+					}
+					keepVisible
+					title={
+						formType === 'password'
+							? 'Change Client Password'
+							: 'Update Client Information'
+					}
+					visible={modalVisible}
+				/>
+			)}
+		</>
+	);
+}
+
+export default DetailActions;
--- src/containers/clients/detail.tsx
+++ src/containers/clients/detail.tsx
@@ -1,66 +1,29 @@
-import { ButtonType, InfoComp } from 'kite-react-tailwind';
+import { InfoComp } from 'kite-react-tailwind';
 import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
 import React from 'react';
-import {
-	FaCheckCircle,
-	FaTimesCircle,
-	FaUser,
-	FaUserCheck,
-	FaUserEdit,
-	FaUserShield,
-	FaUserSlash,
-	FaLock,
-	FaTrash,
-} from 'react-icons/fa';
+import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
 
 import Container from '../../components/common/container';
 import InfoTopBar from '../../components/common/info-topbar';
-import {
-	permissions,
-	CLIENT_OBJECT_PERMISSIONS_PAGE_URL,
-	DEFAULT_IMAGE,
-	USER_PAGE_URL,
-} from '../../config';
-import { useAlertContext, useAuthContext } from '../../store/contexts';
-import {
-	useDeleteClientMutation,
-	useGetClientQuery,
-} from '../../store/queries/clients';
-import { useGetUserObjectPermissionsQuery } from '../../store/queries/permissions';
-import { useActivateUserMutation } from '../../store/queries/users';
+import { DEFAULT_IMAGE } from '../../config/static';
+import { useGetClientQuery } from '../../store/queries/clients';
 import { ClientType, UserObjPermType } from '../../types';
-import { hasModelPermission, getDate, toCapitalize } from '../../utils';
+import { getDate, toCapitalize } from '../../utils';
 
-const DynamicChangePasswordForm = dynamic<any>(
+const DynamicDetailActions = dynamic<any>(
 	() =>
-		import('../../components/employees/detail/change-password-form').then(
+		import('../../components/clients/detail-actions').then(
 			(mod) => mod.default
 		),
 	{
 		loading: () => (
-			<p className="text-center text-gray-500 text-sm md:text-base">
-				Loading Form...
-			</p>
+			<div className="flex items-center justify-center p-4 w-full md:h-1/2 md:mt-auto md:pb-0 md:w-2/3">
+				<p className="animate animate-pulse duration-300 text-center text-gray-800 text-sm transition transform">
+					Loading Actions...
+				</p>
+			</div>
 		),
-		ssr: false,
-	}
-);
-const DynamicUpdateForm = dynamic<any>(
-	() =>
-		import('../../components/clients/update-form').then((mod) => mod.default),
-	{
-		loading: () => (
-			<p className="text-center text-gray-500 text-sm md:text-base">
-				Loading Form...
-			</p>
-		),
-		ssr: false,
-	}
-);
-const DynamicModal = dynamic<any>(
-	() => import('../../components/common/modal').then((mod) => mod.default),
-	{
 		ssr: false,
 	}
 );
@@ -75,16 +38,11 @@ const ClientDetail = ({
 	objUserPerm: UserObjPermType;
 }) => {
 	const router = useRouter();
-
-	const id = router.query.id as string;
-
-	const [modalVisible, setModalVisible] = React.useState(false);
-	const [formType, setFormType] = React.useState<'client' | 'password'>(
-		'client'
-	);
-
-	const { open: showAlert } = useAlertContext();
-	const { data: authData } = useAuthContext();
+	const id = React.useMemo(() => router.query.id as string, [router]);
+	const detailActionsRef = React.useRef<{
+		refreshPerm: () => void;
+		refreshUserPerm: () => void;
+	}>(null);
 
 	const { data, error, isLoading, isFetching, refetch } = useGetClientQuery(
 		{ id },
@@ -94,173 +52,6 @@ const ClientDetail = ({
 			},
 		}
 	);
-
-	const {
-		data: objPermData,
-		isLoading: permLoading,
-		refetch: objPermRefetch,
-	} = useGetUserObjectPermissionsQuery(
-		{
-			modelName: 'clients',
-			objectId: id,
-		},
-		{
-			initialData() {
-				return objPerm;
-			},
-		}
-	);
-
-	// check if the user has edit user permission
-	const { data: objUserPermData, refetch: objUserPermRefetch } =
-		useGetUserObjectPermissionsQuery(
-			{
-				modelName: 'users',
-				objectId: data?.contact.id || '',
-			},
-			{
-				enabled: data && !!data.contact.id,
-				initialData() {
-					return objUserPerm;
-				},
-			}
-		);
-
-	const [canEditUser, canViewUser] = React.useMemo(() => {
-		let canEdit = false;
-		let canView = false;
-
-		// Check model permissions
-		if (authData && (authData.isAdmin || authData.isSuperUser)) {
-			canEdit =
-				!!authData.isSuperUser ||
-				(!!authData.isAdmin &&
-					hasModelPermission(authData.permissions, [permissions.user.EDIT]));
-		}
-		if (authData && (authData.isAdmin || authData.isSuperUser)) {
-			canView =
-				!!authData.isSuperUser ||
-				(!!authData.isAdmin &&
-					hasModelPermission(authData.permissions, [permissions.user.VIEW]));
-		}
-
-		// If the user doesn't have model edit permissions, then check obj edit permission
-		if (!canEdit && objUserPermData) canEdit = objUserPermData.edit;
-		if (!canView && objUserPermData) canView = objUserPermData.view;
-
-		return [canEdit, canView];
-	}, [authData, objUserPermData]);
-
-	const { deleteClient, isLoading: delLoading } = useDeleteClientMutation({
-		onSuccess() {
-			router.back();
-		},
-		onError({ message }) {
-			showAlert({ type: 'success', message });
-		},
-	});
-
-	const { activate, isLoading: loading } = useActivateUserMutation();
-
-	const actions: ButtonType[] = React.useMemo(() => {
-		if (!data || !authData) return [];
-		const buttons: ButtonType[] = [];
-		const canEdit =
-			authData.isSuperUser ||
-			hasModelPermission(authData.permissions, [permissions.client.EDIT]) ||
-			(!permLoading && objPermData && objPermData.edit);
-		const canDelete =
-			authData.isSuperUser ||
-			hasModelPermission(authData.permissions, [permissions.client.DELETE]) ||
-			(!permLoading && objPermData && objPermData.delete);
-		const canViewObjectPermissions =
-			authData.isSuperUser ||
-			(authData.isAdmin &&
-				hasModelPermission(authData.permissions, [
-					permissions.permissionobject.VIEW,
-				]));
-
-		if (canViewUser)
-			buttons.push({
-				bg: 'bg-green-600 hover:bg-green-500',
-				iconLeft: FaUser,
-				link: USER_PAGE_URL(data.contact.id),
-				title: 'Contact Information',
-			});
-		if (canEdit)
-			buttons.push({
-				onClick: () => {
-					formType !== 'client' && setFormType('client');
-					setModalVisible(true);
-				},
-				disabled: loading || delLoading,
-				iconLeft: FaUserEdit,
-				title: 'Edit Client',
-			});
-		if (canEditUser)
-			buttons.push(
-				{
-					bg: 'bg-yellow-600 hover:bg-yellow-500',
-					iconLeft: FaLock,
-					disabled: loading || delLoading,
-					onClick: () => {
-						formType !== 'password' && setFormType('password');
-						setModalVisible(true);
-					},
-					title: 'Change Password',
-				},
-				{
-					bg: data.contact.isActive
-						? 'bg-gray-500 hover:bg-gray-600'
-						: 'bg-green-500 hover:bg-green-600',
-					disabled: loading || delLoading,
-					onClick: () =>
-						data?.contact
-							? activate(
-									[data.contact.email],
-									data.contact.isActive ? 'deactivate' : 'activate'
-							  )
-							: () => {},
-					iconLeft: data.contact.isActive ? FaUserSlash : FaUserCheck,
-					title: data.contact.isActive
-						? loading
-							? 'Deactivating...'
-							: 'Deactivate Client'
-						: loading
-						? 'Activating...'
-						: 'Activate Client',
-				}
-			);
-		if (canDelete)
-			buttons.push({
-				bg: 'bg-red-600 hover:bg-red-500',
-				iconLeft: FaTrash,
-				disabled: loading || delLoading,
-				onClick: () => deleteClient(data.id),
-				title: delLoading ? 'Deleting Client...' : 'Delete Client',
-			});
-		if (canViewObjectPermissions)
-			buttons.push({
-				bg: 'bg-gray-600 hover:bg-gray-500',
-				iconLeft: FaUserShield,
-				link: CLIENT_OBJECT_PERMISSIONS_PAGE_URL(id),
-				title: 'View Record Permissions',
-			});
-		return buttons;
-	}, [
-		activate,
-		authData,
-		data,
-		canEditUser,
-		canViewUser,
-		deleteClient,
-		delLoading,
-		formType,
-		permLoading,
-		objPermData,
-		id,
-		loading,
-	]);
 
 	return (
 		<Container
@@ -278,9 +69,11 @@ const ClientDetail = ({
 			}
 			refresh={{
 				onClick: () => {
+					if (detailActionsRef.current?.refreshPerm)
+						detailActionsRef.current.refreshPerm();
+					if (detailActionsRef.current?.refreshUserPerm)
+						detailActionsRef.current.refreshUserPerm();
 					refetch();
-					objPermRefetch();
-					objUserPermRefetch();
 				},
 				loading: isFetching,
 			}}
@@ -296,7 +89,16 @@ const ClientDetail = ({
 							`${data?.contact.firstName} ${data?.contact.lastName}`
 						)}
 						image={data?.contact.profile?.image?.url || DEFAULT_IMAGE}
-						actions={actions}
+						actions={
+							<DynamicDetailActions
+								data={data}
+								objPerm={objPerm}
+								objUserPerm={objUserPerm}
+								forwardedRef={{
+									ref: detailActionsRef,
+								}}
+							/>
+						}
 					/>
 
 					<div className="mt-4">
@@ -369,48 +171,6 @@ const ClientDetail = ({
 							title="contact & support information"
 						/>
 					</div>
-					<DynamicModal
-						close={() => setModalVisible(false)}
-						component={
-							formType === 'client' && data !== undefined ? (
-								<DynamicUpdateForm
-									onSuccess={() => {
-										showAlert({
-											type: 'success',
-											message: 'Client was updated successfully!',
-										});
-										setModalVisible(false);
-									}}
-									client={data}
-								/>
-							) : formType === 'password' && data ? (
-								<DynamicChangePasswordForm
-									onSuccess={() => {
-										setModalVisible(false);
-										showAlert({
-											type: 'success',
-											message: 'Password changed successfully!',
-										});
-									}}
-									email={data.contact.email}
-								/>
-							) : (
-								<></>
-							)
-						}
-						description={
-							formType === 'password'
-								? 'Fill the form to change client password'
-								: 'Fill in the form to update client information'
-						}
-						keepVisible
-						title={
-							formType === 'password'
-								? 'Change Client Password'
-								: 'Update Client Information'
-						}
-						visible={modalVisible}
-					/>
 				</>
 			)}
 		</Container>
