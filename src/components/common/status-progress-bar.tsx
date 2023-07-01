2c77c8ea533e0c2e0d6b99101374d7dc389af021
kingkite <kitewebdevelopers@gmail.com>
2023-07-01 09:33:24 +0100

    Updated the status progres bar

--- src/components/common/status-progress-bar.tsx
+++ src/components/common/status-progress-bar.tsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from "react";
-import { useFadeIn } from "../../hooks";
+import React from 'react';
+
+import useFadeIn from '../../hooks/useFadeIn';
 
 const StatusProgressBar = ({
 	background,
-	border = "border",
-	borderColor = "border-gray-300",
-	containerColor = "bg-gray-100",
+	border = 'border',
+	borderColor = 'border-gray-300',
+	containerColor = 'bg-gray-100',
 	title,
 	result,
 	value,
@@ -16,29 +17,35 @@ const StatusProgressBar = ({
 	containerColor?: string;
 	title: string;
 	result: number;
-	value: number | string
+	value: number | string | React.ReactNode;
 }) => {
-	const [width, setWidth] = useState(0);
+	const [width, setWidth] = React.useState(0);
 	const { ref, visible } = useFadeIn<HTMLDivElement>(true);
 
-	useEffect(() => {
+	React.useEffect(() => {
 		if (visible) setWidth(result * 100);
 	}, [result, visible]);
 
 	return (
-		<div className={`${border} ${borderColor} ${containerColor} px-3 py-2 rounded-lg w-full`}>
+		<div
+			className={`${border} ${borderColor} ${containerColor} px-3 py-2 rounded-lg w-full`}
+		>
 			<div className="flex items-center justify-between my-1 w-full">
 				<span className="capitalize font-semibold inline-block text-gray-900 text-sm">
 					{title}
 				</span>
-				<span className="font-semibold inline-block text-gray-900 text-sm">
-					{value}
-				</span>
+				{['number', 'string'].includes(typeof value) ? (
+					<span className="font-semibold inline-block text-gray-900 text-sm">
+						{value}
+					</span>
+				) : (
+					value
+				)}
 			</div>
 			<div className="bg-gray-400 h-1 my-1 rounded-lg w-full">
 				<div
 					ref={ref}
-					className={`${background} duration-1000 h-full transform transition-all`}
+					className={`${background} duration-1000 h-full rounded-lg transform transition-all`}
 					style={{ width: `${width < 0 ? 0 : width > 100 ? 100 : width}%` }}
 				/>
 			</div>
