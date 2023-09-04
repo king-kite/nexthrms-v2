29ca98a3d6f7f79e8d1f651770df0bea61f88308
kingkite <kitewebdevelopers@gmail.com>
2023-09-04 20:27:50 +0100

    Added function to get the route parameters as a string

--- src/validators/pagination.ts
+++ src/validators/pagination.ts
@@ -1,32 +1,33 @@
 import type { NextApiRequest } from 'next';
 
 import { ParamsType } from '../types';
-import { getOffsetDate } from '../utils/dates';
+import { getDate } from '../utils/dates';
 
-export function validateParams(
-	query: NextApiRequest['query'] | ParamsType
-): ParamsType {
-	const limit =
-		query.limit && !isNaN(Number(query.limit))
-			? Number(query.limit)
-			: undefined;
-	const offset =
-		query.offset && !isNaN(Number(query.offset)) ? Number(query.offset) : 0;
+export function getRouteParams(query: NextApiRequest['query'] | ParamsType) {
+	const { limit, offset, search, from, to, date } = validateParams(query);
+	const params = `?limit=${limit || ''}&offset=${offset || ''}&from=${from || ''}&to=${
+		to || ''
+	}&search=${search || ''}&date=${date || ''}`;
+
+	return params;
+}
+
+export function validateParams(query: NextApiRequest['query'] | ParamsType): ParamsType {
+	const limit = query.limit && !isNaN(Number(query.limit)) ? Number(query.limit) : undefined;
+	const offset = query.offset && !isNaN(Number(query.offset)) ? Number(query.offset) : 0;
 	const search =
-		typeof query.search === 'string' && query.search.trim() !== ''
-			? query.search
-			: undefined;
+		typeof query.search === 'string' && query.search.trim() !== '' ? query.search : undefined;
 	const from =
 		query.from && (typeof query.from === 'string' || query.from instanceof Date)
-			? (getOffsetDate(query.from) as Date)
+			? (getDate(query.from) as Date)
 			: undefined;
 	const to =
 		query.to && (typeof query.to === 'string' || query.to instanceof Date)
-			? (getOffsetDate(query.to) as Date)
+			? (getDate(query.to) as Date)
 			: undefined;
 	const date =
 		query.date && (typeof query.date === 'string' || query.date instanceof Date)
-			? (getOffsetDate(query.date) as Date)
+			? (getDate(query.date) as Date)
 			: undefined;
 	return { limit, offset, search, from, to, date };
 }
