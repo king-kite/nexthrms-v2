35bd18f7052448900949afb3da8ffc4f96d01a1e
kingkite <kitewebdevelopers@gmail.com>
2023-09-04 16:39:37 +0100

    Changed the pages api route to fetch data the backend instead

--- src/pages/api/notifications/[id].ts
+++ src/pages/api/notifications/[id].ts
@@ -1,109 +1,22 @@
-import prisma from '../../../db';
-import { auth } from '../../../middlewares';
-import { NextErrorMessage } from '../../../utils/classes';
+import axios from 'axios';
 
-export default auth()
-	.use(async (req, res, next) => {
-		const id = req.query.id as string;
-		const notification = await prisma.notification.findUnique({
-			where: {
-				id,
-			},
-			select: {
-				recipientId: true,
-			},
-		});
+import { NOTIFICATION_URL } from '../../../config/services';
+import handler from '../../../middlewares';
 
-		if (!notification)
-			throw new NextErrorMessage(
-				404,
-				'Notification with specified ID does not exist'
-			);
-
-		if (notification.recipientId !== req.user.id)
-			throw new NextErrorMessage(403);
-		next();
-	})
-	.get(async (req, res) => {
-		const notification = await prisma.notification.findUnique({
-			where: {
-				id: req.query.id as string,
-			},
-			select: {
-				createdAt: true,
-				id: true,
-				title: true,
-				message: true,
-				messageId: true,
-				read: true,
-				recipient: {
-					select: {
-						firstName: true,
-						lastName: true,
-						email: true,
-						profile: {
-							select: {
-								image: true,
-							},
-						},
-					},
-				},
-				sender: {
-					select: {
-						firstName: true,
-						lastName: true,
-						email: true,
-						profile: {
-							select: {
-								image: true,
-							},
-						},
-					},
-				},
-				type: true,
-			},
-		});
-		if (!notification)
-			throw new NextErrorMessage(
-				404,
-				'Notification with the specified was not found!'
-			);
-
-		return res.status(200).json({
-			status: 'success',
-			message: 'Fetched notification successfully.',
-			data: notification,
-		});
+export default handler()
+	.get(async function (req, res) {
+		const response = await axios.get(NOTIFICATION_URL((req.query.id as string).toString()));
+		return res.status(200).json(response.data);
 	})
 	.put(async (req, res) => {
 		const { read } = req.body;
 
-		await prisma.notification.update({
-			where: {
-				id: req.query.id as string,
-			},
-			data: {
-				read: !!read,
-			},
-			select: { id: true },
+		const response = await axios.put(NOTIFICATION_URL((req.query.id as string).toString()), {
+			read: !!read,
 		});
-
-		return res.status(200).json({
-			status: 'success',
-			message: 'Notification updated successfully',
-		});
+		return res.status(200).json(response.data);
 	})
-	.delete(async (req, res) => {
-		const id = req.query.id as string;
-
-		await prisma.notification.delete({
-			where: {
-				id,
-			},
-		});
-
-		return res.status(200).json({
-			status: 'success',
-			message: 'Notification was deleted successfully!',
-		});
+	.delete(async function (req, res) {
+		const response = await axios.delete(NOTIFICATION_URL((req.query.id as string).toString()));
+		return res.status(200).json(response.data);
 	});
--- src/pages/api/notifications/index.ts
+++ src/pages/api/notifications/index.ts
@@ -1,68 +1,14 @@
-import prisma from '../../../db';
-import { auth } from '../../../middlewares';
+import axios from 'axios';
+
+import { NOTIFICATIONS_URL } from '../../../config/services';
+import handler from '../../../middlewares';
 import { validateParams } from '../../../validators';
 
-export default auth().get(async (req, res) => {
+export default handler().get(async function (req, res) {
 	const { limit, offset, from, to } = validateParams(req.query);
-	const [total, notifications] = await prisma.$transaction([
-		prisma.notification.count({
-			where: { recipientId: req.user.id },
-		}),
-		prisma.notification.findMany({
-			skip: offset,
-			take: limit,
-			where: {
-				recipientId: req.user.id,
-				createdAt:
-					from && to
-						? {
-								gte: from,
-								lte: to,
-						  }
-						: undefined,
-			},
-			orderBy: {
-				createdAt: 'desc' as const,
-			},
-			select: {
-				createdAt: true,
-				id: true,
-				title: true,
-				message: true,
-				messageId: true,
-				read: true,
-				recipient: {
-					select: {
-						firstName: true,
-						lastName: true,
-						email: true,
-						profile: {
-							select: {
-								image: true,
-							},
-						},
-					},
-				},
-				sender: {
-					select: {
-						firstName: true,
-						lastName: true,
-						email: true,
-						profile: {
-							select: {
-								image: true,
-							},
-						},
-					},
-				},
-				type: true,
-			},
-		}),
-	]);
 
-	return res.status(200).json({
-		status: 'success',
-		message: 'Fetch notifications successfully. A total of ' + total,
-		data: { total, result: notifications },
-	});
+	const params = `?limit=${limit}&offset=${offset}&from=${from}&to=${to}`;
+
+	const response = await axios.get(NOTIFICATIONS_URL + params);
+	return res.status(200).json(response.data.data);
 });
