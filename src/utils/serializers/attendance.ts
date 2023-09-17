05e9677303de1b3c84d5861cc68a02810f7f917c
kingkite <kitewebdevelopers@gmail.com>
2023-09-17 12:19:26 +0100

    The statistics column shows the correct hours spent

--- src/utils/serializers/attendance.ts
+++ src/utils/serializers/attendance.ts
@@ -1,9 +1,6 @@
-import {
-	getDate,
-	getFirstDateOfMonth,
-	getLastDateOfMonth,
-	getNoOfDays,
-} from '../dates';
+import { Dayjs } from 'dayjs';
+
+import { getDate, getFirstDateOfMonth, getLastDateOfMonth, getNoOfDays } from '../dates';
 import { AttendanceInfoType } from '../../types';
 
 type TimeDataType = {
@@ -23,10 +20,15 @@ function getTotalDays(_date: Date | string) {
 	// Loop through the number of days and number of days to work
 	// If the user does not work on all days
 	let totalDays = 0;
+
 	for (let i = 1; i <= numberOfDays; i++) {
-		const date = new Date(firstDate.getFullYear(), firstDate.getMonth(), i);
+		let month: string | number = firstDate.getMonth() + 1;
+		month = month > 9 ? month : month.toString().padStart(2, '0');
+		let day: string | number = firstDate.getDate();
+		day = day > 9 ? day : day.toString().padStart(2, '0');
+		const date = getDate(`${firstDate.getFullYear()}-${month}-${day}`, 'dayjs') as Dayjs;
 		// not sunday
-		if (date.getDay() !== 0) totalDays++;
+		if (date.get('day') !== 0) totalDays++;
 	}
 	// days * hours = total hours;
 	return totalDays;
@@ -37,21 +39,22 @@ export const totalWeekHours = 6 * totalDayHours;
 
 // get the total working hours in a month
 // i.e total days * hours per day = total hours;
-export const totalMonthHours = (date: Date | string) =>
-	getTotalDays(date) * totalDayHours;
+export const totalMonthHours = (date: Date | string) => getTotalDays(date) * totalDayHours;
 
 // get the total overtime hours
 // i.e. If an employee has overtime every working day,
 // how many hours from closing time till midnight is possible for e.g. 6
-export const totalOvertimeMonthHours = (date: Date | string) =>
-	getTotalDays(date) * 6;
+export const totalOvertimeMonthHours = (date: Date | string) => getTotalDays(date) * 6;
 
 // Set the closing time to 6pm at 1970-01-01
-const closingTime = new Date(0);
-closingTime.setHours(18, 0, 0, 0);
+const closingTime = (getDate(0, 'dayjs') as Dayjs)
+	.set('hour', 18)
+	.set('minute', 0)
+	.set('second', +0)
+	.set('millisecond', 0)
+	.toDate();
 
-const todayDate = new Date();
-todayDate.setHours(0, 0, 0, 0);
+const todayDate = getDate(getDate(undefined, true), 'dayjs') as Dayjs;
 
 // get hours info spent on a date
 export function getHours({
@@ -62,7 +65,10 @@ export function getHours({
 }: AttendanceInfoType): TimeDataType {
 	const date = getDate(_date) as Date;
 	// date's hours would have been set to 0 upon creation ie.ie setHours(0, 0, 0, 0)
-	const isTodayDate = date.getTime() === todayDate.getTime();
+	const isTodayDate =
+		todayDate.isSame(date, 'date') &&
+		todayDate.isSame(date, 'month') &&
+		todayDate.isSame(date, 'year');
 
 	// If not today date and no punch out, employee forgot to punch out
 	if (!isTodayDate && !originalPunchOut)
@@ -78,26 +84,47 @@ export function getHours({
 
 	// Get the total hours spent from the punchIn to the closingTime
 	// i.e. the normal hours
-	let normal = (closingTime.getTime() - punchIn.getTime()) / (1000 * 60 * 60);
+	let normal: number = 0;
+
+	if (originalPunchOut) {
+		// Only use the closing time if the person clocks out past the closing time
+		const punchOut = getDate(originalPunchOut) as Date;
+		if (punchOut.getTime() > closingTime.getTime())
+			normal = (closingTime.getTime() - punchIn.getTime()) / (1000 * 60 * 60);
+		else normal = (punchOut.getTime() - punchIn.getTime()) / (1000 * 60 * 60);
+	} else if (isTodayDate) {
+		// If the user is yet to clock out
+		// Get the current time
+		const currentTime = (getDate(undefined, 'dayjs') as Dayjs)
+			.set('year', 1970)
+			.set('month', 0)
+			.set('date', 1)
+			.toDate();
+		normal = (currentTime.getTime() - punchIn.getTime()) / (1000 * 60 * 60);
+	}
+
 	// Make sure normal hours spent is not greater than the totalDayHours
 	normal = normal > totalDayHours ? totalDayHours : normal < 0 ? 0 : normal;
 
 	// percentage hours spent at work
 	const percentage = (normal / totalDayHours) * 100;
 
-	if (!isOvertime)
+	if (!isOvertime) return { normal, overtime: 0, percentage, overtimePercentage: 0 };
+
+	const currentTime = (getDate(undefined, 'dayjs') as Dayjs)
+		.set('year', 1970)
+		.set('month', 0)
+		.set('date', 1)
+		.toDate();
+
+	const punchOut = originalPunchOut ? (getDate(originalPunchOut) as Date) : currentTime; // if today date and no punch means the day is yet to end
+
+	// Check if the punchOut time is less than closing time and return 0 for overtime
+	if (punchOut.getTime() <= closingTime.getTime())
 		return { normal, overtime: 0, percentage, overtimePercentage: 0 };
 
-	const punchOutDate = new Date();
-	punchOutDate.setFullYear(1970, 0, 1); // Changed to 0
-
-	const punchOut = originalPunchOut
-		? (getDate(originalPunchOut) as Date)
-		: punchOutDate; // if today date and no punch means the day is yet to end
-
 	// Get the total hours spent from the closingTime to punchOut time if there is overtime
-	const overtime =
-		(punchOut.getTime() - closingTime.getTime()) / (1000 * 60 * 60);
+	const overtime = (punchOut.getTime() - closingTime.getTime()) / (1000 * 60 * 60);
 	const overtimePercentage = (overtime / totalOvertimeMonthHours(date)) * 100;
 
 	return { normal, overtime, percentage, overtimePercentage };
